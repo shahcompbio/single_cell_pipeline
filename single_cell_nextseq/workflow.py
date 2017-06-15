3d34b0eba88272ed3d122cc47d1b0720602354ee
dranew <andrew.mcpherson@gmail.com>
2017-06-15 16:16:20 -0700

    mem

--- single_cell_nextseq/workflow.py
+++ single_cell_nextseq/workflow.py
@@ -121,7 +121,7 @@ def create_alignment_workflow(
 
     workflow.transform(
         name='bam_collect_wgs_metrics',
-        ctx={'mem': 16},
+        ctx={'mem': 24},
         func=single_cell_nextseq.tasks.bam_collect_wgs_metrics,
         args=(
             mgd.InputFile(bam_filename),
@@ -146,7 +146,7 @@ def create_alignment_workflow(
 
     workflow.transform(
         name='bam_collect_insert_metrics',
-        ctx={'mem': 16},
+        ctx={'mem': 24},
         func=single_cell_nextseq.tasks.bam_collect_insert_metrics,
         args=(
             mgd.InputFile(bam_filename),
