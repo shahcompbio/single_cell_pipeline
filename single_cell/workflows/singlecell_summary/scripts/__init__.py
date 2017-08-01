c16ee7e13a10fde5373e6bc643b4c819fbd3f6dd
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2017-08-01 10:02:30 -0700

    fixing imports

--- single_cell/workflows/singlecell_summary/scripts/__init__.py
+++ single_cell/workflows/singlecell_summary/scripts/__init__.py
@@ -7,6 +7,5 @@ from merge_tsv import MergeFiles
 from plot_heatmap import PlotHeatmap
 from plot_hmmcopy import GenHmmPlots
 from plot_metrics import PlotMetrics
-from workflows.hmmcopy.scripts.filter_data import FilterHmmData
 from summary_metrics import SummaryMetrics
 from plot_kernel_density import PlotKernelDensity
