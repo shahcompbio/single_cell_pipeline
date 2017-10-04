2e8f519c1a7063888341d426b9de4da26d976566
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2017-10-04 14:17:26 -0700

    added import to scripts/__init__ for the new class

--- single_cell/workflows/hmmcopy/scripts/__init__.py
+++ single_cell/workflows/hmmcopy/scripts/__init__.py
@@ -9,3 +9,4 @@ from extract_quality_metrics import ExtractHmmMetrics
 from gen_cn_matrix import GenerateCNMatrix
 from filter_data import FilterHmmData
 from plot_hmmcopy import GenHmmPlots
+from convert_csv_to_seg import ConvertCSVToSEG
