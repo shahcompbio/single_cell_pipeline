f6b5174cd6282fa9ad8dee5bd84978b54d585922
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2017-11-29 14:12:28 -0800

    minor: only run realignment if needed, removed the copy section of code
    Former-commit-id: f2cf4ca164f1de9ece67ce0a1f6e7889b24e174a

--- single_cell/workflows/realignment/__init__.py
+++ single_cell/workflows/realignment/__init__.py
@@ -23,55 +23,39 @@ def create_realignment_workflow(input_bams, input_bais, output_bams, config,
         value=sample_ids,
     )
 
-    if realign:
+    chromosomes = config["chromosomes"]
 
-        chromosomes = config["chromosomes"]
+    workflow.setobj(
+        obj=mgd.OutputChunks('chrom'),
+        value=chromosomes,
+    )
 
-        workflow.setobj(
-            obj=mgd.OutputChunks('chrom'),
-            value=chromosomes,
+    workflow.transform(
+        name='realignment',
+        axes=('chrom',),
+        ctx={'mem': config['med_mem']},
+        func=tasks.realign,
+        args=(
+            mgd.InputFile('bam', 'sample_id', fnames=input_bams),
+            mgd.InputFile('bai', 'sample_id', fnames=input_bais),
+            mgd.TempOutputFile('realigned.bam', 'chrom', 'sample_id'),
+            mgd.TempSpace('realignment_temp', 'chrom', cleanup='before'),
+            config,
+            mgd.InputInstance('chrom')
         )
+    )
 
-        workflow.transform(
-            name='realignment',
-            axes=('chrom',),
-            ctx={'mem': config['med_mem']},
-            func=tasks.realign,
-            args=(
-                mgd.InputFile('bam', 'sample_id', fnames=input_bams),
-                mgd.InputFile('bai', 'sample_id', fnames=input_bais),
-                mgd.TempOutputFile('realigned.bam', 'chrom', 'sample_id'),
-                mgd.TempSpace('realignment_temp', 'chrom', cleanup='before'),
-                config,
-                mgd.InputInstance('chrom')
-            )
+    workflow.transform(
+        name='merge_realignment',
+        ctx={'mem': config['med_mem']},
+        axes=('sample_id',),
+        func=tasks.merge_realignment,
+        args=(
+            mgd.TempInputFile('realigned.bam', 'chrom', 'sample_id'),
+            mgd.OutputFile('bam_realn', 'sample_id', fnames=output_bams),
+            config,
+            mgd.InputInstance('sample_id')
         )
-
-        workflow.transform(
-            name='merge_realignment',
-            ctx={'mem': config['med_mem']},
-            axes=('sample_id',),
-            func=tasks.merge_realignment,
-            args=(
-                mgd.TempInputFile('realigned.bam', 'chrom', 'sample_id'),
-                mgd.OutputFile('bam_realn', 'sample_id', fnames=output_bams),
-                config,
-                mgd.InputInstance('sample_id')
-            )
-        )
-
-    else:
-        workflow.transform(
-            name='copy_realign',
-            axes=('sample_id',),
-            ctx={'mem': config['low_mem']},
-            func=tasks.copy_files,
-            args=(
-                mgd.InputFile('bam', 'sample_id',
-                              fnames=input_bams),
-                mgd.OutputFile('bam_copy', 'sample_id',
-                               fnames=output_bams),
-            )
-        )
+    )
 
     return workflow
