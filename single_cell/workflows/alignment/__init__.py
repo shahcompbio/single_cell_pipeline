1a73e4f94d52cfe3f4e84c796babd1797ffdcca2
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2017-12-07 11:28:50 -0800

    minor: KRONOS-448 moved parallelization to the alignment and merge subworkflows instead of run

--- single_cell/workflows/alignment/__init__.py
+++ single_cell/workflows/alignment/__init__.py
@@ -14,39 +14,53 @@ def create_alignment_workflow(
         fastq_2_filename,
         bam_filename,
         ref_genome,
-        lane_id,
-        sample_id,
+        sample_lanes,
         config,
         args,
         seqinfo):
 
     out_dir = args['out_dir']
-    fastqc_reports = os.path.join(out_dir, 'fastqc',  "{}_{}_reports.tar.gz".format(lane_id, sample_id))
+
+    fastqc_reports = os.path.join(out_dir, 'fastqc', "{lane}_{sample_id}_reports.tar.gz")
+
 
     metrics_dir = os.path.join(args['out_dir'], 'metrics_per_lane',
-                               lane_id, 'flagstat')
-    flagstat_metrics_filename = os.path.join(metrics_dir,'{}.txt'.format(sample_id))
+                               '{lane}', 'flagstat')
+    flagstat_metrics_filename = os.path.join(metrics_dir,'{sample_id}.txt')
 
-    read_group = tasks.get_readgroup(lane_id, sample_id, args, config, seqinfo)
+    bam_filename = dict([(key, bam_filename(key[0], key[1]))
+                        for key in sample_lanes])
+
 
     workflow = pypeliner.workflow.Workflow()
+
+    workflow.setobj(
+        obj=mgd.OutputChunks('sample_id', 'lane'),
+        value=fastq_1_filename.keys(),
+    )
+
+    workflow.setobj(
+        obj=mgd.TempOutputObj('seqinfo', 'sample_id', axes_origin=[]),
+        value=seqinfo)
 
     workflow.transform(
         name='align_reads',
         ctx={'mem': config['med_mem']},
+        axes = ('sample_id', 'lane',),
         func=tasks.align_pe,
         args=(
-            mgd.InputFile(fastq_1_filename),
-            mgd.InputFile(fastq_2_filename),
-            mgd.OutputFile(bam_filename),
-            mgd.OutputFile(fastqc_reports),
-            mgd.OutputFile(flagstat_metrics_filename),
-            mgd.TempSpace('alignment_temp'),
+            mgd.InputFile('fastq_1', 'sample_id', 'lane', fnames=fastq_1_filename),
+            mgd.InputFile('fastq_2', 'sample_id', 'lane', fnames=fastq_2_filename),
+            mgd.OutputFile('aligned_per_cell_per_lane.sorted.bam', 'sample_id', 'lane', fnames=bam_filename),
+            mgd.OutputFile(fastqc_reports, 'sample_id', 'lane'),
+            mgd.OutputFile(flagstat_metrics_filename, 'sample_id', 'lane'),
+            mgd.TempSpace('alignment_temp', 'sample_id', 'lane'),
             ref_genome,
             config,
-            read_group,
-            seqinfo,
-            sample_id
+            mgd.TempInputObj('seqinfo', 'sample_id'),
+            mgd.InputInstance('sample_id'),
+            mgd.InputInstance('lane'),
+            args
         )
     )
 
