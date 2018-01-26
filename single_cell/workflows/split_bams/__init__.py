7c7713ea01b9fe39cd8477db19e7bbcf0b70b139
Diljot <dgrewal@bccrc.ca>
2018-01-26 09:25:48 -0800

    feature: flag to switch between one split job and multiple jobs

--- single_cell/workflows/split_bams/__init__.py
+++ single_cell/workflows/split_bams/__init__.py
@@ -33,6 +33,8 @@ def create_split_workflow(
     normal_split_bai = dict([(ival, normal_split_bai[ival])
                          for ival in intervals])
 
+    one_split_job = config["one_split_job"]
+
 
     workflow = pypeliner.workflow.Workflow()
 
@@ -41,34 +43,64 @@ def create_split_workflow(
         value=intervals,
     )
 
-    workflow.transform(
-        name='split_tumour_bam',
-        ctx={'mem': config['memory']['low']},
-        func=tasks.split_bam_file,
-        axes=('interval',),
-        args=(
-            mgd.InputFile(tumour_bam),
-            mgd.InputFile(tumour_bai),
-            ref_genome,
-            mgd.OutputFile("tumour.split.bam", "interval", fnames=tumour_split_bam),
-            mgd.OutputFile("tumour.split.bam.bai", "interval", fnames=tumour_split_bai),
-            mgd.InputInstance('interval')
+    if one_split_job:
+        workflow.transform(
+            name='split_tumour_bam',
+            ctx={'mem': config['memory']['low']},
+            func=tasks.split_bam_file_one_job,
+            args=(
+                mgd.InputFile(tumour_bam),
+                mgd.InputFile(tumour_bai),
+                ref_genome,
+                mgd.OutputFile("tumour.split.bam", "interval", fnames=tumour_split_bam, axes_origin=[]),
+                mgd.OutputFile("tumour.split.bam.bai", "interval", fnames=tumour_split_bai, axes_origin=[]),
+                intervals
+            )
         )
-    )
- 
-    workflow.transform(
-        name='split_normal_bam',
-        ctx={'mem': config['memory']['low']},
-        axes=('interval',),
-        func=tasks.split_bam_file,
-        args=(
-            mgd.InputFile(normal_bam),
-            mgd.InputFile(normal_bai),
-            ref_genome,
-            mgd.OutputFile("normal.split.bam", "interval", fnames=normal_split_bam),
-            mgd.OutputFile("normal.split.bam.bai", "interval", fnames=normal_split_bai),
-            mgd.InputInstance('interval')
+
+        workflow.transform(
+            name='split_normal_bam',
+            ctx={'mem': config['memory']['low']},
+            func=tasks.split_bam_file_one_job,
+            args=(
+                mgd.InputFile(normal_bam),
+                mgd.InputFile(normal_bai),
+                ref_genome,
+                mgd.OutputFile("normal.split.bam", "interval", fnames=normal_split_bam, axes_origin=[]),
+                mgd.OutputFile("normal.split.bam.bai", "interval", fnames=normal_split_bai, axes_origin=[]),
+                intervals
+            )
         )
-    )
+
+    else:
+        workflow.transform(
+            name='split_tumour_bam',
+            ctx={'mem': config['memory']['low']},
+            func=tasks.split_bam_file,
+            axes=('interval',),
+            args=(
+                mgd.InputFile(tumour_bam),
+                mgd.InputFile(tumour_bai),
+                ref_genome,
+                mgd.OutputFile("tumour.split.bam", "interval", fnames=tumour_split_bam),
+                mgd.OutputFile("tumour.split.bam.bai", "interval", fnames=tumour_split_bai),
+                mgd.InputInstance('interval')
+            )
+        )
+
+        workflow.transform(
+            name='split_normal_bam',
+            ctx={'mem': config['memory']['low']},
+            axes=('interval',),
+            func=tasks.split_bam_file,
+            args=(
+                mgd.InputFile(normal_bam),
+                mgd.InputFile(normal_bai),
+                ref_genome,
+                mgd.OutputFile("normal.split.bam", "interval", fnames=normal_split_bam),
+                mgd.OutputFile("normal.split.bam.bai", "interval", fnames=normal_split_bai),
+                mgd.InputInstance('interval')
+            )
+        )
 
     return workflow
--- single_cell/workflows/split_bams/tasks.py
+++ single_cell/workflows/split_bams/tasks.py
@@ -17,3 +17,20 @@ def split_bam_file(bam, bai, reference, outbam, outbai, interval):
     pypeliner.commandline.execute(
         'samtools', 'index', outbam,
         outbai)
+
+def split_bam_file_one_job(bam, bai, reference, outbam, outbai, intervals):
+
+    for interval in intervals:
+        output_bam = outbam(interval)
+        output_bai = outbai(interval)
+
+        interval = interval.split('_')
+        interval = interval[0] +':'+interval[1] +'-'+interval[2]
+
+        pypeliner.commandline.execute(
+            'samtools', 'view', '-b', bam, interval,
+            '>', output_bam)
+
+        pypeliner.commandline.execute(
+            'samtools', 'index', output_bam,
+            output_bai)
