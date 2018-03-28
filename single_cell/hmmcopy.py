3aad7613b89f7729697fda44117ffb27bd4e529a
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-03-28 11:54:32 -0700

    improvement: can specify multiple modes on cmdline, merged summary and hmmcopy
    Former-commit-id: 49775c3770d767dfaf9e5989c6645b6515c0455e

--- single_cell/hmmcopy.py
+++ single_cell/hmmcopy.py
@@ -8,6 +8,9 @@ import pypeliner
 import pypeliner.managed as mgd
 from workflows import hmmcopy 
 from utils import helpers
+from workflows import singlecell_summary 
+
+
 
 def hmmcopy_workflow(workflow, args):
 
@@ -28,7 +31,6 @@ def hmmcopy_workflow(workflow, args):
         metrics_filename = os.path.join(results_dir, '{}_metrics.csv'.format(args['library_id']))
         params_filename = os.path.join(results_dir, '{}_params.csv'.format(args['library_id']))
 
-
         workflow.subworkflow(
             name='hmmcopy_workflow_' + name,
             func=hmmcopy.create_hmmcopy_workflow,
@@ -45,5 +47,39 @@ def hmmcopy_workflow(workflow, args):
                 results_dir
             ),
         )
+
+        #assume these exist from prev runs for now
+        alignment_metrics = os.path.join(args["out_dir"], "metrics",'alignment_metrics.txt')
+        gc_metrics = os.path.join(args["out_dir"], "metrics",'gc_metrics.txt')
+
+        reads_pdf_output = os.path.join(results_dir, 'plots', '{}_reads.pdf'.format(args['library_id']))
+        segs_pdf_output = os.path.join(results_dir, 'plots', '{}_segs.pdf'.format(args['library_id']))
+        bias_pdf_output = os.path.join(results_dir, 'plots', '{}_bias.pdf'.format(args['library_id']))
+        params_pdf_output = os.path.join(results_dir, 'plots', '{}_params.pdf'.format(args['library_id']))
+
+        # merge all samples per lane together
+        workflow.subworkflow(
+            name='summary_workflow_' + name,
+            func=singlecell_summary.create_summary_workflow,
+            args=(
+                mgd.InputFile(alignment_metrics),
+                mgd.InputFile(gc_metrics),
+                mgd.InputFile(segs_filename),
+                mgd.InputFile(reads_filename),
+                mgd.InputFile(metrics_filename),
+                mgd.InputFile(params_filename),
+                mgd.OutputFile(reads_pdf_output),
+                mgd.OutputFile(segs_pdf_output),
+                mgd.OutputFile(bias_pdf_output),
+                mgd.OutputFile(params_pdf_output),
+                config,
+                params,
+                results_dir,
+                args,
+                sampleids
+            ),
+        )
+
+
 
     return workflow
