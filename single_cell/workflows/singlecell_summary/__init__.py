1fcad72709421d8fb21a4387a5da2d5ba9f0e898
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-04-03 11:59:50 -0700

    minor: passing the reads_per_bin filter to heatmap
    Former-commit-id: 0a40bf18bcc422927a9518d58d726036dd7b1c60

--- single_cell/workflows/singlecell_summary/__init__.py
+++ single_cell/workflows/singlecell_summary/__init__.py
@@ -157,7 +157,7 @@ def create_summary_workflow(alignment_metrics, gc_metrics, hmm_segments, hmm_rea
             'column_name': 'state',
             'plot_by_col': 'experimental_condition',
             'numreads_threshold': config['plot_numreads_threshold'],
-            'quality_threshold': config['plot_median_hmmcopy_reads_per_bin_threshold'],
+            'median_hmmcopy_reads_per_bin_threshold': config['plot_median_hmmcopy_reads_per_bin_threshold'],
             'mad_threshold': config['plot_mad_threshold'],
             'chromosomes': chromosomes,
             'max_cn':hmmparams['num_states'],
