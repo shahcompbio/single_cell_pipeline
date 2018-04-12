a93d4f2d141da32fc746e5ed853329392bad06a3
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-04-12 14:29:34 -0700

    added version tag to the pool names in config

--- single_cell/config/config_generator.py
+++ single_cell/config/config_generator.py
@@ -2,19 +2,37 @@ import yaml
 import json
 import copy
 import os
+import single_cell
+
+
+def get_version(reference):
+    if reference.get("version", None):
+        return reference["version"]
+
+    version = single_cell.__version__
+
+    # strip setuptools metadata
+    version = version.split("+")[0]
+
+    # batch doesnt support . in pool names
+    version = version.replace('.', '_')
+
+    return version
+
+
 def eval_expression(fields, reference):
 
     if not fields.startswith('{') or not fields.endswith('}'):
         return fields
 
-    #remove braces
+    # remove braces
     fields = fields[1:-1]
     fields = fields.split(":")
 
     orig_ref = copy.deepcopy(reference)
 
     for field in fields:
-        field = orig_ref[field[1:]] if field.startswith('$') else field 
+        field = orig_ref[field[1:]] if field.startswith('$') else field
 
         if isinstance(reference, dict):
             reference = reference[field]
@@ -22,6 +40,7 @@ def eval_expression(fields, reference):
             raise Exception("Could not resolve: {}".format(':'.join(fields)))
 
     return reference
+
 
 def go_to_leafs(datadict, reference):
     def recurse_dict(data, reference):
@@ -33,19 +52,34 @@ def go_to_leafs(datadict, reference):
                 recurse_dict(val, reference)
             else:
                 if isinstance(val, list):
-                    data[key] = [eval_expression(element, reference) for element in val]
+                    data[key] = [
+                        eval_expression(
+                            element,
+                            reference) for element in val]
                 elif isinstance(val, str):
                     data[key] = eval_expression(val, reference)
                 elif type(val) in [int, float, bool] or not val:
                     continue
                 else:
                     raise Exception("Unknown {}".format(val))
-    
-
 
     recurse_dict(datadict, reference)
-    
+
     return datadict
+
+
+def add_version_to_pools(cfgdict, reference):
+
+    version = get_version(reference)
+
+    if not cfgdict.get("pools", None):
+        return cfgdict
+
+    for poolid, poolname in cfgdict["pools"].iteritems():
+        cfgdict["pools"][poolid] = '_'.join((poolname, version))
+
+    return cfgdict
+
 
 def main(output=None, input_params=None):
 
@@ -54,14 +88,16 @@ def main(output=None, input_params=None):
     params = os.path.join(cfgdir, "params.yaml")
 
     config = yaml.load(open(config))
-    
+
     params = yaml.load(open(params))
 
     if input_params:
         params.update(input_params)
-    
+
     resolvedpaths = go_to_leafs(config, params)
-    
+
+    resolvedpaths = add_version_to_pools(resolvedpaths, params)
+
     if output:
         with open(output, 'w') as outfile:
             data = yaml.dump(resolvedpaths, default_flow_style=False)
@@ -71,12 +107,11 @@ def main(output=None, input_params=None):
 
 if __name__ == "__main__":
 
-    ##DEBUG ONLY
+    # DEBUG ONLY
     input_params = '{"cluster": "azure", "aligner": "bwa-aln", "reference": "grch37", "smoothing_function": "modal"}'
     input_params = json.loads(input_params)
-    
+
     config = "config.yaml"
     params = "params.yaml"
 
-    
-    print main(config, params, input_params=input_params)
+    print main(input_params=input_params)
