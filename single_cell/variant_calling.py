89d761f01bb0a42115da49f75f0ae1e80cc27b40
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-04-12 09:24:47 -0700

    variant calling changes

--- single_cell/variant_calling.py
+++ single_cell/variant_calling.py
@@ -6,7 +6,7 @@ Created on Feb 22, 2018
 
 import os
 import pypeliner
-import pypeliner.managed as mgd
+import mgd as mgd
 from workflows import snv_postprocessing
 from workflows import mutationseq 
 from workflows import split_bams
@@ -17,7 +17,6 @@ def variant_calling_workflow(workflow, args):
 
     config = helpers.load_config(args)
 
-
     bam_files, bai_files  = helpers.get_bams(args['input_yaml'])
 
     cellids = helpers.get_samples(args['input_yaml'])
@@ -25,13 +24,11 @@ def variant_calling_workflow(workflow, args):
     varcalls_dir = os.path.join(args['out_dir'], 'results',
                                 'variant_calling')
 
-
     wgs_bam_template = args["tumour_template"]
     wgs_bai_template = args["tumour_template"] + ".bai"
 
     normal_bam_template = args["normal_template"]
     normal_bai_template = args["normal_template"] + ".bai"
-
 
     workflow.setobj(
         obj=mgd.OutputChunks('cell_id'),
@@ -57,7 +54,6 @@ def variant_calling_workflow(workflow, args):
 
 
     museq_vcf = os.path.join(varcalls_dir, 'museq_snv.vcf')
-    museq_csv = os.path.join(varcalls_dir, 'museq_snv.csv')
     workflow.subworkflow(
         name='museq',
         func=mutationseq.create_museq_workflow,
@@ -68,7 +64,6 @@ def variant_calling_workflow(workflow, args):
             mgd.InputFile("merged_bam", "region", template=wgs_bai_template, axes_origin=[]),
             config['ref_genome'],
             mgd.OutputFile(museq_vcf),
-            mgd.OutputFile(museq_csv),
             config,
             mgd.TempInputObj('region')
         ),
@@ -76,8 +71,6 @@ def variant_calling_workflow(workflow, args):
 
     strelka_snv_vcf = os.path.join(varcalls_dir, 'strelka_snv.vcf')
     strelka_indel_vcf = os.path.join(varcalls_dir, 'strelka_indel.vcf')
-    strelka_snv_csv = os.path.join(varcalls_dir, 'strelka_snv.csv')
-    strelka_indel_csv = os.path.join(varcalls_dir, 'strelka_indel.csv')
     workflow.subworkflow(
         name='strelka',
         func=strelka.create_strelka_workflow,
@@ -89,30 +82,65 @@ def variant_calling_workflow(workflow, args):
             config['ref_genome'],
             mgd.OutputFile(strelka_indel_vcf),
             mgd.OutputFile(strelka_snv_vcf),
-            mgd.OutputFile(strelka_indel_csv),
-            mgd.OutputFile(strelka_snv_csv),
             config,
             mgd.TempInputObj('region')
         ),
     )
 
-    countdata = os.path.join(varcalls_dir, 'counts.csv')
-    olp_calls = os.path.join(varcalls_dir, 'overlapping_calls.csv')
-    workflow.subworkflow(
-        name='snv_postprocessing',
-        func=snv_postprocessing.create_snv_postprocessing_workflow,
+    workflow.transform(
+        name='merge_snvs',
+        ctx=big_mem_ctx,
+        func=biowrappers.components.io.vcf.tasks.merge_vcfs,
         args=(
-            mgd.InputFile('bam_markdups', 'cell_id', fnames=bam_files),
-            mgd.InputFile('bam_markdups_index', 'cell_id', fnames=bai_files),
             [
-                mgd.InputFile(museq_csv),
-                mgd.InputFile(strelka_snv_csv),
+                mgd.InputFile(museq_vcf),
+                mgd.InputFile(strelka_snv_vcf),
             ],
-            mgd.OutputFile(countdata),
-            mgd.OutputFile(olp_calls),
-            cellids,
-            config,
+            mgd.TempOutputFile('all.snv.vcf')
         )
     )
+
+    workflow.transform(
+        name='finalise_snvs',
+        func=biowrappers.components.io.vcf.tasks.finalise_vcf,
+        args=(
+            mgd.TempInputFile('all.snv.vcf'),
+            mgd.TempOutputFile('all.snv.vcf.gz')
+        )
+    )
+
+    workflow.subworkflow(
+        name='annotate_snvs',
+        axes=(),
+        func=biowrappers.pipelines.snv_call_and_annotate.create_annotation_workflow,
+        args=(
+            config,
+            mgd.TempInputFile('all.snv.vcf.gz'),
+            mgd.TempOutputFile('snv_annotations.h5'),
+            os.path.join(varcalls_dir, 'snv'),
+        ),
+        kwargs={
+            'variant_type': 'snv'
+        }
+    )
+
+    # countdata = os.path.join(varcalls_dir, 'counts.csv')
+    # olp_calls = os.path.join(varcalls_dir, 'overlapping_calls.csv')
+    # workflow.subworkflow(
+    #     name='snv_postprocessing',
+    #     func=snv_postprocessing.create_snv_postprocessing_workflow,
+    #     args=(
+    #         mgd.InputFile('bam_markdups', 'cell_id', fnames=bam_files),
+    #         mgd.InputFile('bam_markdups_index', 'cell_id', fnames=bai_files),
+    #         [
+    #             mgd.InputFile(museq_csv),
+    #             mgd.InputFile(strelka_snv_csv),
+    #         ],
+    #         mgd.OutputFile(countdata),
+    #         mgd.OutputFile(olp_calls),
+    #         cellids,
+    #         config,
+    #     )
+    # )
 
     return workflow
--- single_cell/workflows/mutationseq/__init__.py
+++ single_cell/workflows/mutationseq/__init__.py
@@ -8,8 +8,9 @@ import pypeliner
 import pypeliner.managed as mgd
 import tasks
 
-def create_museq_workflow(normal_bam, normal_bai, tumour_bam, tumour_bai, ref_genome, snv_vcf, snv_csv,
-                          config, regions):
+def create_museq_workflow(
+        normal_bam, normal_bai, tumour_bam, tumour_bai, ref_genome, snv_vcf,
+        config, regions):
 
     workflow = pypeliner.workflow.Workflow()
 
@@ -18,41 +19,31 @@ def create_museq_workflow(normal_bam, normal_bai, tumour_bam, tumour_bai, ref_ge
         value=regions,
     )
 
-
-    workflow.transform(name='run_museq',
-                         ctx={'mem': config["memory"]['med'], 'pool_id': config['pools']['standard'], 'ncpus':1},
-                         axes=('region',),
-                         func=tasks.run_museq,
-                         args=(
-                               mgd.InputFile("merged_bam", "region", fnames=tumour_bam),
-                               mgd.InputFile("merged_bai", "region", fnames=tumour_bai),
-                               mgd.InputFile("normal.split.bam", "region", fnames=normal_bam),
-                               mgd.InputFile("normal.split.bam.bai", "region", fnames=normal_bai),
-                               mgd.TempOutputFile("museq.vcf", "region"),
-                               mgd.TempOutputFile("museq.log", "region"),
-                               config,
-                               mgd.InputInstance("region"),
+    workflow.transform(
+        name='run_museq',
+        ctx={'mem': config["memory"]['med'], 'pool_id': config['pools']['standard'], 'ncpus':1},
+        axes=('region',),
+        func=tasks.run_museq,
+        args=(
+            mgd.InputFile("merged_bam", "region", fnames=tumour_bam),
+            mgd.InputFile("merged_bai", "region", fnames=tumour_bai),
+            mgd.InputFile("normal.split.bam", "region", fnames=normal_bam),
+            mgd.InputFile("normal.split.bam.bai", "region", fnames=normal_bai),
+            mgd.TempOutputFile("museq.vcf", "region"),
+            mgd.TempOutputFile("museq.log", "region"),
+            config,
+            mgd.InputInstance("region"),
         ),
     )
 
     workflow.transform(name='concatenate_vcfs',
-                         ctx={'mem': config["memory"]['med'], 'pool_id': config['pools']['standard'], 'ncpus':1},
-                         func=tasks.concatenate_vcfs,
-                         args=(
-                               mgd.TempInputFile("museq.vcf", "region"),
-                               mgd.OutputFile(snv_vcf)
+        ctx={'mem': config["memory"]['med'], 'pool_id': config['pools']['standard'], 'ncpus':1},
+        func=tasks.concatenate_vcfs,
+        args=(
+            mgd.TempInputFile("museq.vcf", "region"),
+            mgd.OutputFile(snv_vcf)
         ),
     )
-
-    workflow.transform(
-                         name='parse_museq',
-                         ctx={'mem': config["memory"]['low'], 'pool_id': config['pools']['standard'], 'ncpus':1},
-                         func=tasks.parse_museq,
-                         args=(
-                               mgd.InputFile(snv_vcf),
-                               mgd.OutputFile(snv_csv),
-                               )
-                         )
 
     return workflow
 
