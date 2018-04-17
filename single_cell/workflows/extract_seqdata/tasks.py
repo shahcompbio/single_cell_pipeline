4739620aeb210d76c16fec93a2a511133b45554b
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-04-17 16:26:56 -0700

    improvement: seperated the seqdata extraction and titan workflows, titan workflow now ready
    Former-commit-id: 323093b26cbb463edf5033d722f9da76c1446316

---
+++ single_cell/workflows/extract_seqdata/tasks.py
@@ -0,0 +1,16 @@
+import remixt
+import remixt.seqdataio
+
+
+def create_chromosome_seqdata(seqdata, bam_file, snp_positions, chromosomes,
+                              bam_max_fragment_length, bam_max_soft_clipped,
+                              bam_check_proper_pair):
+
+    for chromosome in chromosomes:
+
+        chrom_seqdata = seqdata[chromosome]
+
+        remixt.seqdataio.create_chromosome_seqdata(
+            chrom_seqdata, bam_file, snp_positions, chromosome,
+            bam_max_fragment_length, bam_max_soft_clipped,
+            bam_check_proper_pair)
