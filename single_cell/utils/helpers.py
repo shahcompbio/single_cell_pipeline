d45aabea499028d29a497c8dd0c647fadd0fae28
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-04-17 09:17:11 -0700

    minor: pep-8 formatting, generate config only if config is not specified in cmdline args

--- single_cell/utils/helpers.py
+++ single_cell/utils/helpers.py
@@ -25,6 +25,7 @@ from multiprocessing.pool import ThreadPool
 from single_cell.config import generate_pipeline_config
 from single_cell.config import generate_batch_config
 
+
 def get_incrementing_filename(path):
     """
     avoid overwriting files. if path exists then return path
@@ -41,37 +42,73 @@ def get_incrementing_filename(path):
     return "{}.{}".format(path, i)
 
 
-def generate_configs_in_temp(args):
-
-    config_yaml = "config.yaml"
-    batch_yaml = "batch.yaml"
+def generate_pipeline_config(args):
 
     pipelineargs = args[args.keys()[0]]
 
+    if pipelineargs.get("config_file", None):
+        return args
+
+    config_yaml = "config.yaml"
     tmpdir = pipelineargs.get("tmpdir", None)
-    #use pypeliner tmpdir to store yaml
+    # use pypeliner tmpdir to store yaml
     if tmpdir:
         config_yaml = os.path.join(tmpdir, config_yaml)
-        batch_yaml = os.path.join(tmpdir, batch_yaml)
     else:
         warnings.warn("no tmpdir specified, generating configs in working dir")
         config_yaml = os.path.join(os.getcwd(), config_yaml)
-        batch_yaml = os.path.join(os.getcwd(), batch_yaml)
 
     config_yaml = get_incrementing_filename(config_yaml)
+
+    params_override = pipelineargs["config_override"]
+
+    generate_pipeline_config.main(
+        output=config_yaml,
+        input_params=params_override)
+
+    for _, mode_args in args.iteritems():
+        mode_args["config_file"] = config_yaml
+
+    return args
+
+
+def generate_submit_config(args):
+
+    pipelineargs = args[args.keys()[0]]
+
+    if pipelineargs.get("submit_config", None):
+        return args
+
+    batch_yaml = "batch.yaml"
+
+    tmpdir = pipelineargs.get("tmpdir", None)
+    # use pypeliner tmpdir to store yaml
+    if tmpdir:
+        batch_yaml = os.path.join(tmpdir, batch_yaml)
+    else:
+        warnings.warn("no tmpdir specified, generating configs in working dir")
+        batch_yaml = os.path.join(os.getcwd(), batch_yaml)
+
     batch_yaml = get_incrementing_filename(batch_yaml)
 
     params_override = pipelineargs["config_override"]
 
-    generate_pipeline_config.main(output=config_yaml, input_params = params_override)
-
-    generate_batch_config.main(output=batch_yaml, input_params = params_override)
+    generate_batch_config.main(output=batch_yaml, input_params=params_override)
 
     for _, mode_args in args.iteritems():
-        mode_args["config_file"] = config_yaml
         mode_args["submit_config"] = batch_yaml
 
     return args
+
+
+def generate_configs_in_temp(args):
+
+    args = generate_pipeline_config(args)
+
+    args = generate_batch_config(args)
+
+    return args
+
 
 def run_in_parallel(worker, args, ncores=None):
 
@@ -90,8 +127,8 @@ def run_in_parallel(worker, args, ncores=None):
     for arg in args:
 
         task = pool.apply_async(args_unpack,
-                         args=(worker, arg),
-                        )
+                                args=(worker, arg),
+                                )
         tasks.append(task)
 
     pool.close()
@@ -99,27 +136,30 @@ def run_in_parallel(worker, args, ncores=None):
 
     [task.get() for task in tasks]
 
-
     pool.terminate()
     del pool
 
 
 def run_cmd(cmd, output=None):
 
-    stdout=PIPE
+    stdout = PIPE
     if output:
-        stdout=open(output, "w")
+        stdout = open(output, "w")
 
     p = Popen(cmd, stdout=stdout, stderr=PIPE)
 
     cmdout, cmderr = p.communicate()
-    retc  = p.returncode
+    retc = p.returncode
 
     if retc:
-        raise Exception("command failed. stderr:{}, stdout:{}".format(cmdout, cmderr))
+        raise Exception(
+            "command failed. stderr:{}, stdout:{}".format(
+                cmdout,
+                cmderr))
 
     if output:
         stdout.close()
+
 
 def load_yaml(path):
     try:
@@ -132,10 +172,10 @@ def load_yaml(path):
     return data
 
 
-
 def symlink(actual_file, symlink):
     if not os.path.exists(symlink):
         os.symlink(actual_file, symlink)
+
 
 def copy_file(infile, output):
     shutil.copy(infile, output)
@@ -175,9 +215,10 @@ def load_bam_chromosome_lengths(file_name, chromosomes=None):
 
 
 def get_bam_regions(bam_file, split_size, chromosomes):
-    chromosome_lengths = load_bam_chromosome_lengths(bam_file, chromosomes=chromosomes)
+    chromosome_lengths = load_bam_chromosome_lengths(
+        bam_file,
+        chromosomes=chromosomes)
     return get_regions(chromosome_lengths, split_size)
-
 
 
 def get_fastqs(fastqs_file):
@@ -185,36 +226,38 @@ def get_fastqs(fastqs_file):
     data = load_yaml(fastqs_file)
 
     for cell in data.keys():
-        assert "fastqs" in data[cell], "couldnt extract fastq file paths from yaml input for cell: {}".format(cell)
+        assert "fastqs" in data[
+            cell], "couldnt extract fastq file paths from yaml input for cell: {}".format(cell)
 
     fastq_1_filenames = dict()
     fastq_2_filenames = dict()
     for cell in data.keys():
         fastqs = data[cell]["fastqs"]
-        
-        for lane,paths in fastqs.iteritems():
-            fastq_1_filenames[(cell,lane)] = paths["fastq_1"]
-            fastq_2_filenames[(cell,lane)] = paths["fastq_2"]
-    
+
+        for lane, paths in fastqs.iteritems():
+            fastq_1_filenames[(cell, lane)] = paths["fastq_1"]
+            fastq_2_filenames[(cell, lane)] = paths["fastq_2"]
+
     return fastq_1_filenames, fastq_2_filenames
+
 
 def get_seqinfo(fastqs_file):
 
     data = load_yaml(fastqs_file)
 
     for cell in data.keys():
-        assert "fastqs" in data[cell], "couldnt extract fastq file paths from yaml input for cell: {}".format(cell)
+        assert "fastqs" in data[
+            cell], "couldnt extract fastq file paths from yaml input for cell: {}".format(cell)
 
     seqinfo = dict()
     for cell in data.keys():
         fastqs = data[cell]["fastqs"]
 
-
-        for lane,paths in fastqs.iteritems():
+        for lane, paths in fastqs.iteritems():
 
             if "source" not in paths:
                 raise Exception("source key missing in cell: {}".format(cell))
-            seqinfo[(cell,lane)] = paths["source"]
+            seqinfo[(cell, lane)] = paths["source"]
 
     return seqinfo
 
@@ -227,7 +270,7 @@ def get_sample_info(fastqs_file):
     data = load_yaml(fastqs_file)
 
     cells = data.keys()
-    
+
     for cell in cells:
         del data[cell]["fastqs"]
         del data[cell]["bam"]
@@ -237,20 +280,23 @@ def get_sample_info(fastqs_file):
 def get_samples(fastqs_file):
 
     data = load_yaml(fastqs_file)
-    
+
     return data.keys()
+
 
 def get_bams(fastqs_file):
 
     data = load_yaml(fastqs_file)
 
     for cell in data.keys():
-        assert "bam" in data[cell], "couldnt extract bam file paths from yaml input for cell: {}".format(cell)
+        assert "bam" in data[
+            cell], "couldnt extract bam file paths from yaml input for cell: {}".format(cell)
 
-    bam_filenames = {cell:data[cell]["bam"] for cell in data.keys()}
-    bai_filenames = {cell:data[cell]["bam"]+".bai" for cell in data.keys()}
+    bam_filenames = {cell: data[cell]["bam"] for cell in data.keys()}
+    bai_filenames = {cell: data[cell]["bam"] + ".bai" for cell in data.keys()}
 
     return bam_filenames, bai_filenames
+
 
 def load_config(args):
 
@@ -261,12 +307,13 @@ def makedirs(directory, isfile=False):
 
     if isfile:
         directory = os.path.dirname(directory)
-    
+
     try:
         os.makedirs(directory)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
