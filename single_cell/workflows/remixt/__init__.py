f0f3d62760f7621599dc95d2a5dd5c32afef3c48
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-04-18 12:04:55 -0700

    minor: added skeleton for remixt workflow

---
+++ single_cell/workflows/remixt/__init__.py
@@ -0,0 +1,15 @@
+'''
+Created on Apr 13, 2018
+
+@author: dgrewal
+'''
+import os
+import pypeliner
+import pypeliner.managed as mgd
+import biowrappers
+import tasks
+
+
+def create_remixt_workflow():
+
+    raise NotImplementedError()
