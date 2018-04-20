c3bdfa526cb97841dd1a371a626dc0c5a8751684
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-04-20 16:37:31 -0700

    ui: simplified command line

--- single_cell/cmdline.py
+++ single_cell/cmdline.py
@@ -32,128 +32,62 @@ class parseRegionTemplate(argparse.Action):
         setattr(args, self.dest, values)
 
 
-def arg_exists(args, key, mode, parser):
-    error_string = "option {} is required in {} mode"
+def add_global_args(parser):
+    pypeliner.app.add_arguments(parser)
 
-    if key not in args:
-        raise parser.error(error_string.format(key, mode))
+    parser.add_argument("--input_yaml",
+                        required=True,
+                        help='''yaml file with fastq files, output bams and cell metadata''')
 
+    parser.add_argument("--library_id",
+                        required=True,
+                        help='''Library id.''')
 
-def check_required_args(allcommands, parser):
+    parser.add_argument("--out_dir",
+                        required=True,
+                        help='''Path to output directory.''')
 
-    for mode, args in allcommands.iteritems():
+    parser.add_argument("--config_file",
+                        required=True,
+                        help='''Path to output directory.''')
 
-        if mode in ["align", "hmmcopy", "aneufinder", "copyclone"]:
-            arg_exists(args, "input_yaml", mode, parser)
-            arg_exists(args, "library_id", mode, parser)
+    parser.add_argument("--config_override",
+                        required=True,
+                        type=json.loads,
+                        help='''json string to override the defaults in config''')
 
-        elif mode in ["merge_bams"]:
-            arg_exists(args, "input_yaml", mode, parser)
-            arg_exists(args, "merged_bam_template", mode, parser)
-
-        elif mode in ["variant_calling"]:
-            arg_exists(args, "input_yaml", mode, parser)
-            arg_exists(args, "tumour_template", mode, parser)
-            arg_exists(args, "normal_template", mode, parser)
-
-        elif mode in ["split_bam"]:
-            arg_exists(args, "split_bam_template", mode, parser)
-            arg_exists(args, "wgs_bam", mode, parser)
-
-        elif mode in ["germline_calling"]:
-            arg_exists(args, "input_yaml", mode, parser)
-            arg_exists(args, "input_template", mode, parser)
-
-        elif mode in ["breakpoint_calling"]:
-            arg_exists(args, "input_yaml", mode, parser)
-
-        elif mode in ["generate_config", "clean_sentinels", "copy_number_calling"]:
-            continue
-        else:
-            raise parser.error("unknown mode: {}".format(mode))
-
-
-def generate_args_by_mode(global_args, commands):
-
-    mode_based_args = {}
-
-    for subcommand in commands:
-
-        mode = subcommand["which"]
-        subcommand.update(global_args)
-
-        if subcommand.get("pipelinedir", None):
-            subcommand["pipelinedir"] = os.path.join(
-                subcommand["pipelinedir"],
-                mode)
-
-        mode_based_args[mode] = subcommand
-
-    return mode_based_args
-
-
-def print_help(globalargs, subcommands):
-    args = sys.argv[1:]
-
-    if "-h" in args or "--help" in args:
-        try:
-            print subcommands.parse_known_args(sys.argv[1:])
-        except:
-            print globalargs.parse_known_args(sys.argv[1:])
-
-
-def parse_all_commands(globalargs, subcommands, parser):
-    globalargs, unknowns = globalargs.parse_known_args()
-
-    globalargs = vars(globalargs)
-
-    i = 0
-    subcommand_args = []
-    while unknowns and i < 20:
-        args, unknowns = subcommands.parse_known_args(unknowns)
-        args = vars(args)
-        subcommand_args.append(args)
-
-    if unknowns:
-        parser.error("unrecognized arguments: {}".format(str(unknowns)))
-
-    return globalargs, subcommand_args
+    return parser
 
 
 def parse_args():
-    subcommandargs = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    subparsers = subcommandargs.add_subparsers()
+    subparsers = parser.add_subparsers()
 
-    # subparser to align bams
-    align = subparsers.add_parser("align")
+    align = add_global_args(subparsers.add_parser("align"))
     align.set_defaults(which='align')
     align.add_argument('--realign',
                        action='store_true',
+                       required=True,
                        help='''will run local realignment on all cells in batch mode''')
 
-    # subparser to run hmmcopy
-    hmmcopy = subparsers.add_parser("hmmcopy")
+    hmmcopy = add_global_args(subparsers.add_parser("hmmcopy"))
     hmmcopy.set_defaults(which='hmmcopy')
 
-    # subparser to align bams
-    copyclone = subparsers.add_parser("copyclone")
+    copyclone = add_global_args(subparsers.add_parser("copyclone"))
     copyclone.set_defaults(which='copyclone')
 
-    # subparser to align bams
-    aneufinder = subparsers.add_parser("aneufinder")
+    aneufinder = add_global_args(subparsers.add_parser("aneufinder"))
     aneufinder.set_defaults(which='aneufinder')
 
-    # subparser to align bams
-    merge_bams = subparsers.add_parser("merge_bams")
+    merge_bams = add_global_args(subparsers.add_parser("merge_bams"))
     merge_bams.set_defaults(which='merge_bams')
     merge_bams.add_argument("--merged_bam_template",
                             required=True,
                             help='''template for saving the bams merged by region, use {} as place holder for genomic region''')
 
-    # subparser to align bams
-    split_bam = subparsers.add_parser("split_bam")
+    split_bam = add_global_args(subparsers.add_parser("split_bam"))
     split_bam.set_defaults(which='split_bam')
 
     split_bam.add_argument("--split_bam_template",
@@ -165,8 +99,7 @@ def parse_args():
                            required=True,
                            help='''path to the whole genome bam file''')
 
-    # subparser to align bams
-    variant_calling = subparsers.add_parser("variant_calling")
+    variant_calling = add_global_args(subparsers.add_parser("variant_calling"))
     variant_calling.set_defaults(which='variant_calling')
 
     variant_calling.add_argument("--tumour_template",
@@ -179,8 +112,7 @@ def parse_args():
                                  action=parseRegionTemplate,
                                  help='''template for saving the bams merged by region, use {} as place holder for genomic region''')
 
-    # subparser to align bams
-    copy_number_calling = subparsers.add_parser("copy_number_calling")
+    copy_number_calling = add_global_args(subparsers.add_parser("copy_number_calling"))
     copy_number_calling.set_defaults(which='copy_number_calling')
 
     copy_number_calling.add_argument("--tumour_yaml",
@@ -196,8 +128,7 @@ def parse_args():
                                      help='''ID to identify the results''')
 
 
-    # subparser to align bams
-    germline_calling = subparsers.add_parser("germline_calling")
+    germline_calling = add_global_args(subparsers.add_parser("germline_calling"))
     germline_calling.set_defaults(which='germline_calling')
 
     germline_calling.add_argument("--input_template",
@@ -205,11 +136,9 @@ def parse_args():
                                   action=parseRegionTemplate,
                                   help='''template for saving the bams merged by region, use {} as place holder for genomic region''')
 
-    # subparser to align bams
-    breakpoint_calling = subparsers.add_parser("breakpoint_calling")
+    breakpoint_calling = add_global_args(subparsers.add_parser("breakpoint_calling"))
     breakpoint_calling.set_defaults(which='breakpoint_calling')
 
-    # subparser to align bams
     generate_config = subparsers.add_parser("generate_config")
     generate_config.set_defaults(which='generate_config')
 
@@ -234,36 +163,6 @@ def parse_args():
                                  required=True,
                                  help='''pattern to clean''')
 
-    globalargs = argparse.ArgumentParser()
-    pypeliner.app.add_arguments(globalargs)
-
-    globalargs.add_argument("--input_yaml",
-                            help='''yaml file with fastq files, output bams and cell metadata''')
-
-    globalargs.add_argument("--library_id",
-                            help='''Library id.''')
-
-    globalargs.add_argument("--out_dir",
-                            help='''Path to output directory.''')
-
-    globalargs.add_argument("--config_file",
-                            help='''Path to output directory.''')
-
-    globalargs.add_argument("--config_override",
-                            type=json.loads,
-                            help='''json string to override the defaults in config''')
-
-    # check if -h in args
-    print_help(globalargs, subcommandargs)
-
-    globalargs, subcommandargs = parse_all_commands(
-        globalargs, subcommandargs, subcommandargs)
-
-    # add config paths to global args if needed.
-    globalargs = helpers.generate_configs_in_temp(globalargs)
-
-    args = generate_args_by_mode(globalargs, subcommandargs)
-
-    check_required_args(args, subcommandargs)
+    args = vars(parser.parse_args())
 
     return args
