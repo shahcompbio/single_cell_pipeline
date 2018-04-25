2da74e2cbd07ee15b85eedead958f5634d8dfa58
Ubuntu <singlecellproduction@singlecellpipelineheadnode.ixyzfwdzouvetgy5ldv3rds1ie.vx.internal.cloudapp.net>
2018-04-25 03:37:31 +0000

    minor: added bai paths to the job

--- single_cell/workflows/extract_seqdata/tasks.py
+++ single_cell/workflows/extract_seqdata/tasks.py
@@ -2,7 +2,7 @@ import remixt
 import remixt.seqdataio
 
 
-def create_chromosome_seqdata(seqdata, bam_file, snp_positions, chromosomes,
+def create_chromosome_seqdata(seqdata, bam_file, bai_file, snp_positions, chromosomes,
                               bam_max_fragment_length, bam_max_soft_clipped,
                               bam_check_proper_pair):
 
