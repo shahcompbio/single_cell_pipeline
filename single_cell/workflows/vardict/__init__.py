1600613e7fe324b4f2cadc3a135ef901857ebffc
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-05-03 11:18:58 -0700

    feature: vardict variant caller

---
+++ single_cell/workflows/vardict/__init__.py
@@ -0,0 +1,66 @@
+import pypeliner
+import biowrappers.components.variant_calling.vardict.tasks
+import biowrappers.components.io.vcf.tasks
+
+
+def create_vardict_paired_sample_workflow(
+        normal_bam_file,
+        tumour_bam_file,
+        ref_genome_fasta_file,
+        out_file,
+        chromosomes=default_chromosomes,
+        java=False,
+        min_allele_frequency=0.01,
+        remove_duplicate_reads=False,
+        sample_names=None):
+
+    workflow = pypeliner.workflow.Workflow(
+        default_ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2, 'pool_id': config['pools']['standard'], 'ncpus':1 })
+
+    workflow.setobj(
+        obj=mgd.OutputChunks('region'),
+        value=normal_bam_file.keys(),
+    )
+
+    workflow.transform(
+        name='run_vardict',
+        axes=('region',),
+        ctx={'mem': 12, 'num_retry': 4, 'mem_retry_increment': 2},
+        func=biowrappers.components.variant_calling.vardict.tasks.run_paired_sample_vardict,
+        args=(
+            pypeliner.managed.InputFile('normal.bam', 'region', fnames=normal_bam_file, extensions=['.bai']),
+            pypeliner.managed.InputFile('tumour.bam', 'region', fnames=tumour_bam_file, extensions=['.bai']),
+            ref_genome_fasta_file,
+            pypeliner.managed.InputInstance('region'),
+            pypeliner.managed.TempOutputFile('result.vcf', 'region'),
+        ),
+        kwargs={
+            'java': java,
+            'min_allele_frequency': min_allele_frequency,
+            'remove_duplicate_reads': remove_duplicate_reads,
+            'sample_names': sample_names,
+        },
+    )
+
+    workflow.transform(
+        name='compress_tmp',
+        axes=('region',),
+        ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
+        func=biowrappers.components.io.vcf.tasks.compress_vcf,
+        args=(
+            pypeliner.managed.TempInputFile('result.vcf', 'region'),
+            pypeliner.managed.TempOutputFile('result.vcf.gz', 'region'),
+        ),
+    )
+
+    workflow.transform(
+        name='concatenate_vcf',
+        ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
+        func=biowrappers.components.io.vcf.tasks.concatenate_vcf,
+        args=(
+            pypeliner.managed.TempInputFile('result.vcf.gz', 'region'),
+            pypeliner.managed.OutputFile(out_file),
+        ),
+    )
+
+    return workflow
