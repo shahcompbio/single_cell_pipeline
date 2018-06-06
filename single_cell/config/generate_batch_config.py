519ca56dc465f056b2abea4dd4ea3f8763e9d560
Diljot Grewal <dgrewal@momac30.bccrc.ca>
2018-06-06 15:37:44 -0700

    feature: reimplemented config and batch generation codee
    Former-commit-id: 95b4c5ab022394a01cab3c8fa01ef056bb68c10d

--- single_cell/config/generate_batch_config.py
+++ single_cell/config/generate_batch_config.py
@@ -1,111 +1,13 @@
 
 import os
-import yaml
-import json
-import re
-import single_cell
 from single_cell.utils import helpers
 import warnings
-
-
-def get_version(reference):
-    if reference.get("version", None):
-        version = reference["version"].replace('.', '_')
-    else:
-        version = single_cell.__version__
-
-        # strip setuptools metadata
-        version = version.split("+")[0]
-
-        # batch doesnt support . in pool names
-        version = version.replace('.', '_')
-
-    reference["version"] = version
-
-
-def eval_expression(fields, reference):
-
-    if not isinstance(fields, str):
-        return fields
-
-    for match in re.finditer("\$\{(.*?)\}", fields):
-
-        match = match.group()
-
-        assert match.startswith("${") and match.endswith("}")
-
-        reference_key = match[2:-1]
-
-#         print reference_key, match, reference[reference_key]
-
-        assert reference_key in reference
-
-        fields = fields.replace(match, reference[reference_key])
-
-    return fields
-
-
-def go_to_leafs(datadict, reference):
-    def recurse_dict(data, reference):
-        '''
-        recurse into dict and yield ylims.
-        '''
-        for key, val in data.iteritems():
-            del data[key]
-            newkey = eval_expression(key, reference)
-            data[newkey] = val
-
-            if isinstance(val, dict):
-                recurse_dict(val, reference)
-            else:
-                if isinstance(val, list):
-                    data[key] = [
-                        eval_expression(
-                            element,
-                            reference) for element in val]
-                elif isinstance(val, basestring):
-                    data[key] = eval_expression(val, reference)
-                elif type(val) in [int, float, bool] or not val:
-                    continue
-                else:
-                    print val
-                    print type(val)
-                    raise Exception("Unknown {}".format(val))
-
-    recurse_dict(datadict, reference)
-
-    return datadict
-
-
-def main(output=None, input_params=None):
-
-    cfgdir = os.path.realpath(os.path.dirname(__file__))
-    config = os.path.join(cfgdir, "batch.yaml")
-    params = os.path.join(cfgdir, "batch_params.yaml")
-
-    config = yaml.load(open(config))
-
-    params = yaml.load(open(params))
-
-    if input_params:
-        params.update(input_params)
-
-    get_version(params)
-
-    resolvedpaths = go_to_leafs(config, params)
-
-    if output:
-        helpers.makedirs(output, isfile=True)
-        with open(output, 'w') as outfile:
-            data = yaml.safe_dump(resolvedpaths, default_flow_style=False)
-            outfile.write(data)
-    else:
-        return resolvedpaths
+import batch
 
 
 def generate_submit_config_in_temp(args):
 
-    if args['which'] in ['clean_sentinels','generate_config']:
+    if args['which'] in ['clean_sentinels', 'generate_config']:
         return args
 
     if args.get("submit_config", None):
@@ -129,14 +31,10 @@ def generate_submit_config_in_temp(args):
 
     params_override = args["config_override"]
 
-    main(output=batch_yaml, input_params=params_override)
+    config_params = batch.get_batch_params(override=params_override)
+    config = batch.get_batch_config(config_params)
+    config.write_config(config, batch_yaml)
 
     args["submit_config"] = batch_yaml
 
     return args
-
-if __name__ == "__main__":
-    input_params = '{"cluster": "azure", "aligner": "bwa-aln", "reference": "grch37", "smoothing_function": "modal"}'
-    input_params = json.loads(input_params)
-
-    main(input_params=input_params, output="out.yaml")
