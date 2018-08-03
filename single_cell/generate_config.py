1048a3b82c1a505e598082688e3597b13ff84d81
Diljot Grewal <dgrewal@bccrc.ca>
2018-08-03 15:39:21 -0700

    bugfix:issues with config generation
    Former-commit-id: e577415b307d959aae12fe6bd962a69b362cda73

--- single_cell/generate_config.py
+++ single_cell/generate_config.py
@@ -4,18 +4,21 @@ Created on Apr 9, 2018
 @author: dgrewal
 '''
 
-import os
-from single_cell.config import generate_pipeline_config
-from single_cell.config import generate_batch_config
+from single_cell.config import pipeline_config
+from single_cell.config import batch
 
 
 def generate_config(args):
-    config_yaml = args["pipeline_config"]
-    batch_yaml = args["batch_config"]
-    params_override = args["config_override"]
+    config_yaml = args.get("pipeline_config")
+    batch_yaml = args.get("batch_config")
+    params_override = args.get("config_override")
 
-    generate_pipeline_config.main(
-        output=config_yaml,
-        input_params=params_override)
+    if config_yaml:
+        config_params = pipeline_config.get_config_params(override=params_override)
+        config = pipeline_config.get_singlecell_pipeline_config(config_params)
+        pipeline_config.write_config(config, config_yaml)
 
-    generate_batch_config.main(output=batch_yaml, input_params=params_override)
+    if batch_yaml:
+        config_params = batch.get_batch_params(override=params_override)
+        config = batch.get_batch_config(config_params)
+        batch.write_config(config, batch_yaml)
