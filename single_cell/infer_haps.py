17dae47d6586ec1de62003e9a69434d19011c6f1
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-09-17 13:54:46 -0700

    feature: haplotypes with allele counts for snps

--- single_cell/infer_haps.py
+++ single_cell/infer_haps.py
@@ -8,7 +8,6 @@ import os
 import pypeliner.managed as mgd
 from single_cell.utils import helpers
 from workflows import extract_seqdata
-import remixt.analysis.haplotype
 
 
 def infer_haps_workflow(workflow, args):
@@ -29,57 +28,18 @@ def infer_haps_workflow(workflow, args):
           }
 
     haps_dir = os.path.join(args["out_dir"], "infer_haps")
-    out_file = os.path.join(haps_dir, "results", "results.h5")
+
+    haplotypes_filename = os.path.join(haps_dir, "results", "haplotypes.tsv")
+    allele_counts_filename = os.path.join(haps_dir, "results", "allele_counts.tsv")
+
+    snp_positions_filename = remixt.config.get_filename(config, ref_data_dir, 'snp_positions')
+    bam_max_fragment_length = remixt.config.get_param(config, 'bam_max_fragment_length')
+    bam_max_soft_clipped = remixt.config.get_param(config, 'bam_max_soft_clipped')
+    bam_check_proper_pair = remixt.config.get_param(config, 'bam_check_proper_pair')
 
     workflow.setobj(
         obj=mgd.OutputChunks('chromosome'),
         value=config['titan_params']['chromosomes']
-    )
-
-    workflow.transform(
-        name="get_snp_positions_filename",
-        ctx=ctx,
-        func="remixt.config.get_filename",
-        ret=mgd.TempOutputObj('snp_positions_filename'),
-        args=(
-              remixt_config,
-              config['titan_params']['ref_data_dir'],
-              'snp_positions'
-        )
-    )
-
-    workflow.transform(
-        name="get_bam_max_fragment_length",
-        ctx=ctx,
-        func="remixt.config.get_param",
-        ret=mgd.TempOutputObj('bam_max_fragment_length'),
-        args=(
-              remixt_config,
-              'bam_max_fragment_length'
-        )
-    )
-
-
-    workflow.transform(
-        name="get_bam_max_soft_clipped",
-        ctx=ctx,
-        func="remixt.config.get_param",
-        ret=mgd.TempOutputObj('bam_max_soft_clipped'),
-        args=(
-              remixt_config,
-              'bam_max_soft_clipped'
-        )
-    )
-
-    workflow.transform(
-        name="get_bam_check_proper_pair",
-        ctx=ctx,
-        func="remixt.config.get_param",
-        ret=mgd.TempOutputObj('bam_check_proper_pair'),
-        args=(
-              remixt_config,
-              'bam_check_proper_pair'
-        )
     )
 
     if args['input_yaml']:
@@ -108,10 +68,10 @@ def infer_haps_workflow(workflow, args):
                 config,
                 config['titan_params'].get('extract_seqdata', {}),
                 config['titan_params']['ref_data_dir'],
-                mgd.TempInputObj('snp_positions_filename'),
-                mgd.TempInputObj('bam_max_fragment_length'),
-                mgd.TempInputObj('bam_max_soft_clipped'),
-                mgd.TempInputObj('bam_check_proper_pair'),
+                snp_positions_filename,
+                bam_max_fragment_length,
+                bam_max_soft_clipped,
+                bam_check_proper_pair,
             )
         )
 
@@ -136,10 +96,10 @@ def infer_haps_workflow(workflow, args):
                 config,
                 config['titan_params'].get('extract_seqdata', {}),
                 config['titan_params']['ref_data_dir'],
-                mgd.TempInputObj('snp_positions_filename'),
-                mgd.TempInputObj('bam_max_fragment_length'),
-                mgd.TempInputObj('bam_max_soft_clipped'),
-                mgd.TempInputObj('bam_check_proper_pair'),
+                snp_positions_filename,
+                bam_max_fragment_length,
+                bam_max_soft_clipped,
+                bam_check_proper_pair,
             ),
             kwargs={'multiprocess': True}
         )
@@ -149,7 +109,7 @@ def infer_haps_workflow(workflow, args):
             name='infer_snp_genotype',
             axes=('chromosome',),
             ctx={'mem': 16},
-            func=remixt.analysis.haplotype.infer_snp_genotype_from_normal,
+            func='remixt.analysis.haplotype.infer_snp_genotype_from_normal',
             args=(
                 mgd.TempOutputFile('snp_genotype.tsv', 'chromosome'),
                 mgd.TempInputFile("seqdata_normal_all_cells_merged.h5"),
@@ -162,7 +122,7 @@ def infer_haps_workflow(workflow, args):
             name='infer_snp_genotype',
             axes=('chromosome',),
             ctx={'mem': 16},
-            func=remixt.analysis.haplotype.infer_snp_genotype_from_tumour,
+            func='remixt.analysis.haplotype.infer_snp_genotype_from_tumour',
             args=(
                 mgd.TempOutputFile('snp_genotype.tsv', 'chromosome'),
                 {'sample': mgd.TempInputFile("seqdata_normal_all_cells_merged.h5")},
@@ -175,25 +135,48 @@ def infer_haps_workflow(workflow, args):
         name='infer_haps',
         axes=('chromosome',),
         ctx={'mem': 16},
-        func=remixt.analysis.haplotype.infer_haps,
+        func='remixt.analysis.haplotype.infer_haps',
         args=(
             mgd.TempOutputFile('haps.tsv', 'chromosome'),
             mgd.TempInputFile('snp_genotype.tsv', 'chromosome'),
             mgd.InputInstance('chromosome'),
             mgd.TempSpace('haplotyping', 'chromosome'),
             config,
-            '/refdata/',
+            config['titan_params']['ref_data_dir'],
         )
     )
 
     workflow.transform(
         name='merge_haps',
         ctx={'mem': 16},
-        func=remixt.utils.merge_tables,
+        func='remixt.utils.merge_tables',
         args=(
-            mgd.OutputFile(out_file),
+            mgd.OutputFile(haplotypes_filename),
             mgd.TempInputFile('haps.tsv', 'chromosome'),
         )
+    )
+
+    workflow.transform(
+        name='create_segments',
+        func='remixt.analysis.segment.create_segments',
+        args=(
+            mgd.TempOutputFile('segments.tsv'),
+            config,
+            config['titan_params']['ref_data_dir'],
+        ),
+    )
+
+    workflow.transform(
+        name='haplotype_allele_readcount',
+        ctx={'mem': 20},
+        func='remixt.analysis.readcount.haplotype_allele_readcount',
+        args=(
+            mgd.OutputFile(allele_counts_filename),
+            mgd.TempInputFile('segments.tsv'),
+            mgd.TempInputFile('tumour.h5', 'cell_id'),
+            mgd.InputFile(haplotypes_filename),
+            config,
+        ),
     )
 
     return workflow
