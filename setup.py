81823e5c1b3f39f7f8f68b5184061dd888d88cc2
Andrew Mcpherson <amcpherson@headnode.ixyzfwdzouvetgy5ldv3rds1ie.vx.internal.cloudapp.net>
2018-10-05 21:18:29 +0000

    minor: docker fixes
    Former-commit-id: 6cac43246a78e2ab3b0bc3a2336ae56c8c691f3e

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author='Andrew McPherson',
     author_email='andrew.mcpherson@gmail.com',
     entry_points={'console_scripts': ['single_cell = single_cell.run:main']},
-    package_data={'':['scripts/*.py', 'scripts/*.R', 'scripts/*.npz', "config/*.yaml"]}
+    package_data={'':['scripts/*.py', 'scripts/*.R', 'scripts/*.npz', "config/*.yaml", "data/*"]}
 )
