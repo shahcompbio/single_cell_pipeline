e879a267f086ed4b616bbe304f8f041bab00fb58
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-11-03 09:23:27 -0700

    refactor: multiple jobs for merge

--- single_cell/workflows/merge_bams/__init__.py
+++ single_cell/workflows/merge_bams/__init__.py
@@ -9,6 +9,7 @@ import pypeliner.managed as mgd
 from single_cell.utils import helpers
 import single_cell
 
+
 def create_merge_bams_workflow(
     input_bams,
     merged_bams,
@@ -80,3 +81,37 @@ def create_merge_bams_workflow(
 
 
     return workflow
+
+
+def create_cell_region_merge_workflow(
+    cell_bams,
+    region_bams,
+    regions,
+    config,
+):
+    workflow = pypeliner.workflow.Workflow()
+
+    workflow.setobj(
+        obj=mgd.OutputChunks('cell_id'),
+        value=cell_bams.keys(),
+    )
+
+    workflow.setobj(
+        obj=mgd.OutputChunks('region'),
+        value=regions,
+    )
+
+    workflow.transform(
+        name='split_merge_tumour',
+        func='single_cell.workflows.merge_bams.tasks.cell_region_merge_bams',
+        axes=('region',),
+        args=(
+            mgd.InputFile('tumour_cells.bam', 'cell_id', extensions=['.bai'], fnames=cell_bams),
+            mgd.OutputFile('tumour_regions.bam', 'region', axes_origin=[], extensions=['.bai'], fnames=region_bams),
+            mgd.Instance('region'),
+            helpers.get_container_ctx(config['containers'], 'samtools'),
+        ),
+    )
+
+    return workflow
+
--- single_cell/workflows/merge_bams/tasks.py
+++ single_cell/workflows/merge_bams/tasks.py
@@ -11,6 +11,20 @@ from single_cell.utils import helpers
 from subprocess import Popen, PIPE
 
 
+def cell_region_merge_bams(cell_bams, region_bam, region, kwargs):
+    cell_bams = cell_bams.values()
+    region = '{}:{}-{}'.format(*region.split('-'))
+
+    bamutils.bam_merge(
+        cell_bams, region_bam,
+        region=region,
+        **kwargs)
+
+    bamutils.bam_index(
+        region_bam, region_bam+'.bai',
+        **kwargs)
+
+
 def merge_bam_worker(input_bam_files, output_bam, region, kwargs):
 
     bamutils.bam_merge(
