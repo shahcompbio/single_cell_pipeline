93a971cf4b78a0dfba0e1a1e67114e7f4be23fb3
Pasha <pashaa@lbst0117.mskcc.org>
2018-11-08 11:54:28 -0500

    add info.yaml

--- single_cell/demultiplex_bam.py
+++ single_cell/demultiplex_bam.py
@@ -4,12 +4,12 @@ Created on November 01, 2018
 @author: pashaa
 '''
 import os
+import glob
 import single_cell
 import pypeliner
 import pypeliner.managed as mgd
 from workflows import demultiplex_bam
 from single_cell.utils import helpers
-from logging.config import fileConfig
 
 
 
@@ -18,12 +18,12 @@ def demultiplex_bam_workflow(workflow, args):
 
     config = helpers.load_config(args)
 
-    out_dir = os.path.join(args["out_dir"], "results", args['bam'])
+    out_dir = os.path.join(args["out_dir"], os.path.basename(args['bam'][:-4]))
 
     info_file = os.path.join(out_dir, "info.yaml")
 
-    # ctx = {'mem_retry_increment': 2, 'ncpus': 1}
-    # ctx.update(helpers.get_container_ctx(config['containers'], 'single_cell_pipeline'))
+    ctx = {'mem_retry_increment': 2, 'ncpus': 1}
+    ctx.update(helpers.get_container_ctx(config['containers'], 'single_cell_pipeline'))
 
     workflow.subworkflow(
         name='create_demultiplex_bam_workflow',
@@ -38,38 +38,33 @@ def demultiplex_bam_workflow(workflow, args):
     )
 
 
+    inputs = {'filename': args['bam']}
 
+    outputs = {'dirname': out_dir}
 
-    # TODO fix input and output dict
-    #inputs = helpers.get_fastq_files(args["input_yaml"])
+    metadata = {
+        'demultiplex_bam': {
+            'name': 'demultiplex_bam',
+            'version': single_cell.__version__,
+            'containers': config['containers'],
+            'output_datasets': outputs,
+            'input_datasets': inputs,
+            'results': {
+                'demultiplexed_fastqs': glob.glob(os.path.join(out_dir, "*.fq"))
+            },
+        }
+    }
 
-    #outputs = {k: helpers.format_file_yaml(v) for k,v in bam_files.iteritems()}
-    # inputs = {}
-    # outputs = {}
-    #
-    # metadata = {
-    #     'demultiplex_bam': {
-    #         'name': 'demultiplex_bam',
-    #         'version': single_cell.__version__,
-    #         'containers': config['containers'],
-    #         'output_datasets': outputs,
-    #         'input_datasets': inputs,
-    #         'results': {
-    #             'demultiplexed_fastqs': helpers.format_file_yaml(out_dir)
-    #         },
-    #     }
-    # }
-    #
-    # workflow.transform(
-    #     name='generate_meta_yaml',
-    #     ctx=dict(mem=config['memory']['med'],
-    #              pool_id=config['pools']['standard'],
-    #              **ctx),
-    #     func="single_cell.utils.helpers.write_to_yaml",
-    #     args=(
-    #         mgd.OutputFile(info_file),
-    #         metadata
-    #     )
-    # )
+    workflow.transform(
+        name='generate_meta_yaml',
+        ctx=dict(mem=config['memory']['med'],
+                 pool_id=config['pools']['standard'],
+                 **ctx),
+        func="single_cell.utils.helpers.write_to_yaml",
+        args=(
+            mgd.OutputFile(info_file),
+            metadata
+        )
+    )
 
     return workflow
