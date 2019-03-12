033263ba5e09e77e5b8fc5fdcd385c0d596a198a
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-03-12 16:11:44 -0700

    update
    Former-commit-id: 16f2496aa2f306f645a999c4214d06e31ba71fab

--- single_cell/workflows/destruct/__init__.py
+++ single_cell/workflows/destruct/__init__.py
@@ -13,21 +13,38 @@ def create_destruct_workflow(
     breakpoints_filename,
     raw_data_directory,
     normal_sample_id='normal',
+    tumour_sample_id='tumour',
 ):
-    bam_files = tumour_bam_files
-    bam_files[normal_sample_id] = normal_bam_file
-
     baseimage = config['docker']['single_cell_pipeline']
 
     workflow = pypeliner.workflow.Workflow()
 
     workflow.setobj(
         obj=mgd.OutputChunks('cell_id'),
-        value=bam_files.keys(),
+        value=tumour_bam_files.keys(),
     )
 
     workflow.commandline(
-        name='bamdisc',
+        name='bamdisc_normal',
+        ctx={'io': 1, 'mem': 8, 'docker_image': baseimage},
+        args=(
+            'destruct_bamdiscordantfastq',
+            '-r',
+            '-c', config['breakpoint_calling']['bam_max_soft_clipped'],
+            '-f', config['breakpoint_calling']['bam_max_fragment_length'],
+            '-b', mgd.InputFile(normal_bam_file),
+            '-s', mgd.TempOutputFile('normal_stats'),
+            '--fastq1', mgd.TempOutputFile('normal_reads_1'),
+            '--fastq2', mgd.TempOutputFile('normal_reads_2'),
+            '-t', mgd.TempSpace('bamdisc_normal_tempspace'),
+            '-n', config['breakpoint_calling']['num_read_samples'],
+            '--sample1', mgd.TempOutputFile('normal_sample_1'),
+            '--sample2', mgd.TempOutputFile('normal_sample_2'),
+        ),
+    )
+
+    workflow.commandline(
+        name='bamdisc_tumour',
         axes=('cell_id',),
         ctx={'io': 1, 'mem': 8, 'docker_image': baseimage},
         args=(
@@ -36,13 +53,13 @@ def create_destruct_workflow(
             '-c', config['breakpoint_calling']['bam_max_soft_clipped'],
             '-f', config['breakpoint_calling']['bam_max_fragment_length'],
             '-b', mgd.InputFile('bam', 'cell_id', fnames=bam_files),
-            '-s', mgd.TempOutputFile('stats', 'cell_id'),
-            '--fastq1', mgd.TempOutputFile('reads_1', 'cell_id'),
-            '--fastq2', mgd.TempOutputFile('reads_2', 'cell_id'),
-            '-t', mgd.TempSpace('bamdisc_tempspace', 'cell_id'),
+            '-s', mgd.TempOutputFile('tumour_stats', 'cell_id'),
+            '--fastq1', mgd.TempOutputFile('tumour_reads_1', 'cell_id'),
+            '--fastq2', mgd.TempOutputFile('tumour_reads_2', 'cell_id'),
+            '-t', mgd.TempSpace('bamdisc_tumour_tempspace', 'cell_id'),
             '-n', config['breakpoint_calling']['num_read_samples'],
-            '--sample1', mgd.TempOutputFile('sample_1', 'cell_id'),
-            '--sample2', mgd.TempOutputFile('sample_2', 'cell_id'),
+            '--sample1', mgd.TempOutputFile('tumour_sample_1', 'cell_id'),
+            '--sample2', mgd.TempOutputFile('tumour_sample_2', 'cell_id'),
         ),
     )
 
@@ -51,10 +68,10 @@ def create_destruct_workflow(
         ctx={'io': 1, 'mem': 8},
         func="single_cell.workflows.destruct.tasks.merge_fastqs",
         args=(
-            mgd.TempInputFile('reads_1', 'cell_id'),
-            mgd.TempInputFile('reads_2', 'cell_id'),
-            mgd.TempOutputFile('reads_1'),
-            mgd.TempOutputFile('reads_2'),
+            mgd.TempInputFile('tumour_reads_1', 'cell_id'),
+            mgd.TempInputFile('tumour_reads_2', 'cell_id'),
+            mgd.TempOutputFile('tumour_reads_1'),
+            mgd.TempOutputFile('tumour_reads_2'),
         ),
     )
 
@@ -63,10 +80,10 @@ def create_destruct_workflow(
         ctx={'io': 1, 'mem': 8},
         func="single_cell.workflows.destruct.tasks.resample_fastqs",
         args=(
-            mgd.TempInputFile('sample_1', 'cell_id'),
-            mgd.TempInputFile('sample_2', 'cell_id'),
-            mgd.TempOutputFile('sample_1'),
-            mgd.TempOutputFile('sample_2'),
+            mgd.TempInputFile('tumour_sample_1', 'cell_id'),
+            mgd.TempInputFile('tumour_sample_2', 'cell_id'),
+            mgd.TempOutputFile('tumour_sample_1'),
+            mgd.TempOutputFile('tumour_sample_2'),
             config['breakpoint_calling']['num_read_samples'],
         ),
     )
@@ -76,8 +93,8 @@ def create_destruct_workflow(
         ctx={'io': 1, 'mem': 8},
         func="single_cell.workflows.destruct.tasks.merge_stats",
         args=(
-            mgd.TempInputFile('stats', 'cell_id'),
-            mgd.TempOutputFile('stats'),
+            mgd.TempInputFile('tumour_stats', 'cell_id'),
+            mgd.TempOutputFile('tumour_stats'),
         ),
     )
 
@@ -86,11 +103,26 @@ def create_destruct_workflow(
         ctx={'docker_image': baseimage},
         func="destruct.workflows.create_destruct_fastq_workflow",
         args=(
-            mgd.TempInputFile('reads_1'),
-            mgd.TempInputFile('reads_2'),
-            mgd.TempInputFile('sample_1'),
-            mgd.TempInputFile('sample_2'),
-            mgd.TempInputFile('stats'),
+            {
+                normal_sample_id: mgd.TempInputFile('normal_reads_1'),
+                tumour_sample_id: mgd.TempInputFile('tumour_reads_1'),
+            },
+            {
+                normal_sample_id: mgd.TempInputFile('normal_reads_2'),
+                tumour_sample_id: mgd.TempInputFile('tumour_reads_2'),
+            },
+            {
+                normal_sample_id: mgd.TempInputFile('normal_sample_1'),
+                tumour_sample_id: mgd.TempInputFile('tumour_sample_1'),
+            },
+            {
+                normal_sample_id: mgd.TempInputFile('normal_sample_2'),
+                tumour_sample_id: mgd.TempInputFile('tumour_sample_2'),
+            },
+            {
+                normal_sample_id: mgd.TempInputFile('normal_stats'),
+                tumour_sample_id: mgd.TempInputFile('tumour_stats'),
+            },
             mgd.TempOutputFile('breakpoint_table'),
             mgd.TempOutputFile('breakpoint_library_table'),
             mgd.TempOutputFile('breakpoint_read_table'),
@@ -102,23 +134,28 @@ def create_destruct_workflow(
         },
     )
 
-
-
+    workflow.transform(
+        name='extract_cell_counts',
+        ctx={'mem': 8},
+        func=
+        args=(
+            mgd.TempInputFile('breakpoint_read_table'),
+            mgd.TempOutputFile('cell_counts'),
+        ),
+    )
 
     workflow.transform(
         name='filter_annotate_breakpoints',
         ctx={'mem': 8},
         func=tasks.filter_annotate_breakpoints,
         args=(
-            pypeliner.managed.InputFile(breakpoint_file),
-            pypeliner.managed.InputFile(breakpoint_library_file),
+            pypeliner.managed.TempInputFile('breakpoint_table'),
+            pypeliner.managed.TempInputFile('breakpoint_library_table'),
+            pypeliner.managed.TempInputFile('cell_counts'),
             [normal_sample_id],
-            pypeliner.managed.OutputFile(somatic_breakpoint_file),
-            pypeliner.managed.OutputFile(somatic_breakpoint_library_file),
+            pypeliner.managed.OutputFile(breakpoints_filename),
         ),
     )
-
-
 
     return workflow
 
--- single_cell/workflows/destruct/tasks.py
+++ single_cell/workflows/destruct/tasks.py
@@ -4,23 +4,67 @@ import gzip
 import pandas as pd
 
 
-def merge_fastqs(input_fastqs_1, input_fastqs_2, output_fastq_1, output_fastq_2):
-    """ Merge a set of pairs of fastqs into a single pair.
+def read_paired_cell_fastqs(input_fastqs_1, input_fastqs_2):
+    """ Generator for fastq entries from paired fastq files.
+
+    Read per cell paired fastq files and for each entry in the
+    matching fastq pair, return the cell id and a list of
+    two lists of fastq lines.
+
+    Args:
+        input_fastqs_1 (dict): fastq filenames keyed by cell_id
+        input_fastqs_2 (dict): fastq filenames keyed by cell_id
+
+    Yields:
+        cell_id (str), fastq_lines (list)
     """
     assert set(input_fastqs_1.keys()) == set(input_fastqs_2.keys())
-    for idx, key in enumerate(input_fastqs_1.keys()):
-        if idx == 0:
-            shutil.copyfile(input_fastqs_1[key], output_fastq_1)
-            shutil.copyfile(input_fastqs_2[key], output_fastq_2)
-        else:
-            with open(input_fastqs_1[key], 'rb') as infile, open(output_fastq_1, 'ab') as outfile:
-                shutil.copyfileobj(infile, outfile)
-            with open(input_fastqs_2[key], 'rb') as infile, open(output_fastq_2, 'ab') as outfile:
-                shutil.copyfileobj(infile, outfile)
+    for idx, cell_id in enumerate(input_fastqs_1.keys()):
+        opener_1 = (open, gzip.open)[input_fastqs_1[cell_id].endswith('.gz')]
+        opener_2 = (open, gzip.open)[input_fastqs_2[cell_id].endswith('.gz')]
+        with opener_1(input_fastqs_1[cell_id], 'r') as file_1, opener_2(input_fastqs_2[cell_id], 'r') as file_2:
+            fastq_lines = [[], []]
+            for fastq_1_line, fastq_2_line in zip(file_1, file_2):
+                fastq_lines[0].append(fastq_1_line.rstrip())
+                fastq_lines[1].append(fastq_2_line.rstrip())
+                if len(fastq_lines[0]) == 4:
+                    yield fastq_lines
+                    fastq_lines = [[], []]
+    assert len(fastq_lines[0]) == 0 or len(fastq_lines[0]) == 4
+    if len(fastq_lines[0]) == 4:
+        yield cell_id, fastq_lines
+
+
+def merge_tag_cell_fastqs(input_fastqs_1, input_fastqs_2, output_fastq_1, output_fastq_2):
+    """ Merge a set of pairs of fastqs into a single pair.
+
+    Merge per cell paired fastq files and modify each pair
+    of reads to have a name that starts with the cell id
+
+    Args:
+        input_fastqs_1 (dict): fastq filenames keyed by cell_id
+        input_fastqs_2 (dict): fastq filenames keyed by cell_id
+        output_fastq_1 (str): output fastq filename
+        output_fastq_2 (str): output fastq filename
+    """
+    fastq_iterator = read_paired_cell_fastqs(input_fastqs_1, input_fastqs_2)
+    with gzip.open(output_fastq_1, 'w') as file_1, gzip.open(output_fastq_2, 'w') as file_2:
+        for cell_id, fastq_lines in fastq_iterator:
+            for read_end in (0, 1):
+                if fastq_lines[read_end][0][0] != '@':
+                    raise ValueError('Expected @ as first character of read name')
+                fastq_lines[read_end][0] = '@' + cell_id + '_' + fastq_lines[read_end][0][1:]
+            for line in fastq_lines[0]:
+                file_1.write(line + '\n')
+            for line in fastq_lines[1]:
+                file_2.write(line + '\n')
 
 
 def random_subset(iterator, K):
     """ Resevoir sampling.
+
+    Given an iterator, sample K items uniformly until
+    the iterator is finished.
     """
     result = []
     N = 0
@@ -37,41 +81,35 @@ def random_subset(iterator, K):
     return result
 
 
-def read_paired_fastqs(input_fastqs_1, input_fastqs_2):
-    """ Generator for fastq entries from paired fastq files.
-    """
-    assert set(input_fastqs_1.keys()) == set(input_fastqs_2.keys())
-    for idx, key in enumerate(input_fastqs_1.keys()):
-        opener_1 = (open, gzip.open)[input_fastqs_1[key].endswith('.gz')]
-        opener_2 = (open, gzip.open)[input_fastqs_2[key].endswith('.gz')]
-        with opener_1(input_fastqs_1[key], 'r') as file_1, opener_2(input_fastqs_2[key], 'r') as file_2:
-            fastq_lines = [[], []]
-            for fastq_1_line, fastq_2_line in zip(file_1, file_2):
-                fastq_lines[0].append(fastq_1_line)
-                fastq_lines[1].append(fastq_2_line)
-                if len(fastq_lines[0]) == 4:
-                    yield fastq_lines
-                    fastq_lines = [[], []]
-    assert len(fastq_lines[0]) == 0 or len(fastq_lines[0]) == 4
-    if len(fastq_lines[0]) == 4:
-        yield fastq_lines
-
-
 def resample_fastqs(input_fastqs_1, input_fastqs_2, output_fastq_1, output_fastq_2, num_reads):
     """ Resample from a set of fastqs.
+
+    Resample a fixed number of reads uniformly from a set of per cell
+    paired fastq files.
+
+    Args:
+        input_fastqs_1 (dict): fastq filenames keyed by cell_id
+        input_fastqs_2 (dict): fastq filenames keyed by cell_id
+        output_fastq_1 (str): output fastq filename
+        output_fastq_2 (str): output fastq filename
+        num_reads (int): number of reads to sample
     """
-    fastq_iterator = read_paired_fastqs(input_fastqs_1, input_fastqs_2)
+    fastq_iterator = read_paired_cell_fastqs(input_fastqs_1, input_fastqs_2)
     fastq_sample = random_subset(fastq_iterator, num_reads)
     with gzip.open(output_fastq_1, 'w') as file_1, gzip.open(output_fastq_2, 'w') as file_2:
-        for fastq_lines in fastq_sample:
+        for cell_id, fastq_lines in fastq_sample:
             for line in fastq_lines[0]:
-                file_1.write(line)
+                file_1.write(line + '\n')
             for line in fastq_lines[1]:
-                file_2.write(line)
+                file_2.write(line + '\n')
 
 
 def merge_stats(input_stats, output_stats):
     """ Merge a set of destruct stats files into one file.
+
+    Args:
+        input_stats (dict): stats filenames keyed by cell_id
+        output_stats (str): output stats filename
     """
     data = []
     for idx, key in enumerate(input_stats.keys()):
@@ -80,5 +118,4 @@ def merge_stats(input_stats, output_stats):
     data = data.groupby(['type', 'key'])['value'].sum().reset_index()
     data.to_csv(output_stats, sep='\t', index=False)
 
-            
 
