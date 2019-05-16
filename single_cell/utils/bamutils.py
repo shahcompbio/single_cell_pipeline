d2ab4d56ff8b930a655b4734166f84d89fc07769
Simong Song <ssm19951222@gmail.com>
2019-05-16 15:03:14 -0700

    gE

--- single_cell/utils/bamutils.py
+++ single_cell/utils/bamutils.py
@@ -159,7 +159,6 @@ def biobloom_categorizer(fastq1, fastq2, tempdir, biobloom_count_metrics, docker
     tempdir = os.path.join(tempdir, 'biobloom')
     if not os.path.exists(tempdir):
         helpers.makedirs(tempdir)
-    biobloomcategorizer = "/home/linuxbrew/.linuxbrew/Cellar/biobloomtools/2.3.2/bin/biobloomcategorizer"
     cmd = [
         biobloomcategorizer,
         "--fq",
