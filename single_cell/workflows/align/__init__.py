8ba11a636b2359f60c0942f4a683852708660b9b
Simong Song <ssm19951222@gmail.com>
2019-05-17 10:16:26 -0700

    improvement: merged alignment workflows, removed metrics_only

--- single_cell/workflows/align/__init__.py
+++ single_cell/workflows/align/__init__.py
@@ -13,6 +13,9 @@ def create_alignment_workflow(
         fastq_1_filename,
         fastq_2_filename,
         bam_filename,
+        alignment_metrics,
+        gc_metrics,
+        plot_metrics,
         ref_genome,
         config,
         args,
@@ -20,8 +23,9 @@ def create_alignment_workflow(
         centerinfo,
         sample_info,
         cell_ids,
-        biobloom_count_metrics):
+):
 
+    disable_biobloom = args['disable_biobloom']
 
     baseimage = config['docker']['single_cell_pipeline']
 
@@ -34,9 +38,6 @@ def create_alignment_workflow(
     bam_filename = dict([(cellid, bam_filename[cellid])
                          for cellid in cell_ids])
 
-    biobloom_count_metrics = dict([(cellid, biobloom_count_metrics[cellid])
-                         for cellid in cell_ids])
-
     chromosomes = config["chromosomes"]
 
     workflow = pypeliner.workflow.Workflow()
@@ -45,6 +46,11 @@ def create_alignment_workflow(
         obj=mgd.OutputChunks('chrom'),
         value=chromosomes,
     )
+
+    workflow.setobj(
+        obj=mgd.TempOutputObj('sampleinfo', 'cell_id', axes_origin=[]),
+        value=sample_info)
+
 
     workflow.setobj(
         obj=mgd.OutputChunks('cell_id', 'lane'),
@@ -58,11 +64,6 @@ def create_alignment_workflow(
     workflow.setobj(
         obj=mgd.TempOutputObj('center', 'cell_id', 'lane', axes_origin=[]),
         value=centerinfo)
-
-    workflow.setobj(
-        obj=mgd.TempOutputObj('sampleinfo', 'cell_id', axes_origin=[]),
-        value=sample_info)
-
 
     fastqc_reports = os.path.join(
         lane_metrics,
@@ -82,6 +83,7 @@ def create_alignment_workflow(
             mgd.TempOutputFile(
                 'aligned_per_cell_per_lane.sorted.bam', 'cell_id', 'lane'),
             mgd.TempOutputFile('biobloom_count_metrics', 'cell_id', 'lane'),
+            disable_biobloom,
             mgd.OutputFile(fastqc_reports, 'cell_id', 'lane'),
             mgd.OutputFile(flagstat_metrics, 'cell_id', 'lane'),
             mgd.TempSpace('alignment_temp', 'cell_id', 'lane'),
@@ -106,7 +108,8 @@ def create_alignment_workflow(
         func="single_cell.workflows.align.tasks.merge_biobloom",
         axes=('cell_id',),
         args=( mgd.TempInputFile('biobloom_count_metrics', 'cell_id', 'lane'),
-               mgd.OutputFile('biobloom_count_metrics', 'cell_id', fnames=biobloom_count_metrics)
+               mgd.TempOutputFile('biobloom_count_metrics_merged', 'cell_id'),
+               disable_biobloom,
                )
     )
 
@@ -179,6 +182,145 @@ def create_alignment_workflow(
             config['docker'],
             mgd.OutputFile(markdups_metrics, 'cell_id'),
             mgd.OutputFile(flagstat_metrics, 'cell_id'),
+        ),
+    )
+
+    wgs_metrics_filename = os.path.join(
+        merge_metrics, 'wgs_metrics', '{cell_id}.wgs_metrics.txt')
+    workflow.transform(
+        name='bam_collect_wgs_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.workflows.align.tasks.bam_collect_wgs_metrics",
+        axes=('cell_id',),
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            ref_genome,
+            mgd.OutputFile(wgs_metrics_filename, 'cell_id'),
+            config['docker'],
+            config['picard_wgs_params'],
+            mgd.TempSpace('wgs_tempdir', 'cell_id'),
+        ),
+    )
+
+    gc_metrics_filename = os.path.join(
+        merge_metrics,
+        'gc_metrics',
+        '{cell_id}.gc_metrics.txt')
+    gc_summary_filename = os.path.join(
+        merge_metrics,
+        'gc_metrics',
+        '{cell_id}.gc_metrics.summ.txt')
+    gc_chart_filename = os.path.join(
+        merge_metrics,
+        'gc_metrics',
+        '{cell_id}.gc_metrics.pdf')
+    workflow.transform(
+        name='bam_collect_gc_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.workflows.align.tasks.bam_collect_gc_metrics",
+        axes=('cell_id',),
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            ref_genome,
+            mgd.OutputFile(gc_metrics_filename, 'cell_id'),
+            mgd.OutputFile(gc_summary_filename, 'cell_id'),
+            mgd.OutputFile(gc_chart_filename, 'cell_id'),
+            mgd.TempSpace('gc_tempdir', 'cell_id'),
+            config['docker'],
+        ),
+    )
+
+    insert_metrics_filename = os.path.join(
+        merge_metrics,
+        'insert_metrics',
+        '{cell_id}.insert_metrics.txt')
+    insert_histogram_filename = os.path.join(
+        merge_metrics,
+        'insert_metrics',
+        '{cell_id}.insert_metrics.pdf')
+    workflow.transform(
+        name='bam_collect_insert_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.workflows.align.tasks.bam_collect_insert_metrics",
+        axes=('cell_id',),
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            mgd.InputFile(flagstat_metrics, 'cell_id'),
+            mgd.OutputFile(insert_metrics_filename, 'cell_id'),
+            mgd.OutputFile(insert_histogram_filename, 'cell_id'),
+            mgd.TempSpace('insefefert_tempdir', 'cell_id'),
+            config['docker'],
+        ),
+    )
+
+    workflow.transform(
+        name="collect_gc_metrics",
+        func="single_cell.workflows.align.tasks.collect_gc",
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        args=(
+            mgd.InputFile(gc_metrics_filename, 'cell_id', axes_origin=[]),
+            mgd.TempOutputFile('gc_metrics.csv.gz', extensions=['.yaml']),
+            mgd.TempSpace("temp_gc")
+        ),
+    )
+
+    workflow.transform(
+        name='collect_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.workflows.align.tasks.collect_metrics",
+        args=(
+            mgd.InputFile(flagstat_metrics, 'cell_id', axes_origin=[]),
+            mgd.InputFile(markdups_metrics, 'cell_id', axes_origin=[]),
+            mgd.InputFile(insert_metrics_filename, 'cell_id', axes_origin=[]),
+            mgd.InputFile(wgs_metrics_filename, 'cell_id', axes_origin=[]),
+            mgd.TempSpace("tempdir_collect_metrics"),
+            mgd.TempOutputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
+            mgd.TempInputFile('biobloom_count_metrics_merged', 'cell_id', axes_origin=[])
+        ),
+    )
+
+    workflow.transform(
+        name='annotate_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.workflows.align.tasks.annotate_metrics",
+        args=(
+            mgd.TempInputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
+            sample_info,
+            mgd.TempOutputFile('alignment_metrics_annotated.csv.gz', extensions=['.yaml']),
+        ),
+    )
+
+    workflow.transform(
+        name='plot_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.workflows.align.tasks.plot_metrics",
+        args=(
+            mgd.TempInputFile('alignment_metrics_annotated.csv.gz', extensions=['.yaml']),
+            mgd.OutputFile(plot_metrics),
+            'QC pipeline metrics',
+            mgd.TempInputFile('gc_metrics.csv.gz', extensions=['.yaml']),
+            config['gc_windows'],
+        )
+    )
+
+    workflow.transform(
+        name='finalize_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.utils.csvutils.finalize_csv",
+        args=(
+            mgd.TempInputFile('alignment_metrics_annotated.csv.gz', extensions=['.yaml']),
+            mgd.OutputFile(alignment_metrics, extensions=['.yaml']),
+        ),
+    )
+
+
+    workflow.transform(
+        name='finalize_gc_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
+        func="single_cell.utils.csvutils.finalize_csv",
+        args=(
+            mgd.TempInputFile('gc_metrics.csv.gz', extensions=['.yaml']),
+            mgd.OutputFile(gc_metrics, extensions=['.yaml']),
         ),
     )
 
--- single_cell/workflows/align/scripts/collect_metrics.py
+++ single_cell/workflows/align/scripts/collect_metrics.py
@@ -7,30 +7,31 @@ import pandas as pd
 import os
 
 class CollectMetrics(object):
-    def __init__(self, wgs_metrics, insert_metrics, flagstat_metrics, markdups_metrics, output, sample_id):
+    def __init__(self, wgs_metrics, insert_metrics, flagstat_metrics, markdups_metrics, output, sample_id, biobloom_count_metrics):
         self.wgs_metrics = wgs_metrics
         self.flagstat_metrics = flagstat_metrics
         self.insert_metrics = insert_metrics
         self.markdups_metrics = markdups_metrics
+        self.biobloom_count_metrics = biobloom_count_metrics
         self.output = output
         self.sample_id = sample_id
-    
+
 
     def extract_wgs_metrics(self):
         """
         get the coverage_depth (mean_coverage column)
         get the coverage_breadth (count/genome_territory)
         """
-    
+
         mfile = open(self.wgs_metrics)
-        
+
         metrics = []
         hist = {}
-        
+
         addmetrics = False
         addhist = False
-       
-       
+
+
         for line in mfile:
             if line.strip() == '':
                 continue
@@ -38,12 +39,12 @@ class CollectMetrics(object):
                 addmetrics = True
                 addhist = False
                 continue
-    
+
             if line.startswith('## HISTOGRAM'):
                 addhist = True
                 addmetrics = False
                 continue
-            
+
             if addmetrics:
                 metrics.append(line.strip().split('\t'))
             if addhist:
@@ -51,98 +52,98 @@ class CollectMetrics(object):
                 if line[0] == 'coverage':
                     continue
                 hist[int(line[0])] = int(line[1])
-    
+
         mfile.close()
         header, data = metrics
-    
+
         header = [v.lower() for v in header]
         header = {v:i for i,v in enumerate(header)}
-    
+
         gen_territory = int(data[header['genome_territory']])
         cov_depth  = float(data[header['mean_coverage']])
         count = int(hist[0])
-        cov_breadth = (gen_territory - count) / gen_territory 
-    
+        cov_breadth = (gen_territory - count) / gen_territory
+
         return cov_breadth, cov_depth
-    
-    
+
+
     def extract_flagstat_metrics(self):
         """
         extract from flagstat
         """
-    
-        df = pd.read_table(self.flagstat_metrics,
+
+        df = pd.read_csv(self.flagstat_metrics,
                            sep=r'\s\+\s0\s',
                            header=None,
                            names=['value', 'type'],
                            engine='python')
-    
+
         tot_reads = df[df['type']=='in total (QC-passed reads + QC-failed reads)']['value']
         tot_mpd_reads = df[(df['type'].str.contains('mapped') == True ) & ( df['type'].str.contains('mate mapped') == False)]
         tot_dup_reads = df[df['type']=='duplicates']['value']
         tot_prop_paired = df[df['type'].str.contains('properly paired') ]
-    
+
         assert len(tot_reads) == 1
         assert len(tot_mpd_reads) == 1
         assert len(tot_dup_reads) == 1
         assert len(tot_prop_paired) == 1
-    
+
         tot_reads = tot_reads.iloc[0]
         tot_mpd_reads = tot_mpd_reads['value'].iloc[0]
         tot_dup_reads = tot_dup_reads.iloc[0]
         tot_prop_paired = tot_prop_paired['value'].iloc[0]
-    
-    
+
+
         return tot_reads, tot_mpd_reads, tot_dup_reads, tot_prop_paired
-    
+
     def extract_duplication_metrics(self):
         """
         extract from markdups
         """
-    
+
         mfile = open(self.markdups_metrics)
-    
+
         targetlines = []
-    
+
         line = mfile.readline()
-    
+
         while line != '':
             if line.startswith('## METRICS CLASS'):
                 targetlines.append(mfile.readline().strip('\n').split('\t'))
                 targetlines.append(mfile.readline().strip('\n').split('\t'))
                 break
             line = mfile.readline()
-    
+
         mfile.close()
-    
+
         header, data = targetlines
-    
+
         header = [v.lower() for v in header]
         header = {v:i for i,v in enumerate(header)}
-    
+
         unprd_mpd_rds = int(data[header['unpaired_reads_examined']])
         prd_mpd_rds = int(data[header['read_pairs_examined']])
         unprd_dup_rds = int(data[header['unpaired_read_duplicates']])
         prd_dup_rds = int(data[header['read_pair_duplicates']])
         unmpd_rds = data[header['unmapped_reads']]
         est_lib_size = data[header['estimated_library_size']]
-    
+
         rd_pair_opt_dup = int(data[header['read_pair_optical_duplicates']])
-    
+
         try:
             perc_dup_reads = (unprd_dup_rds + ((prd_dup_rds + rd_pair_opt_dup) * 2)) / (unprd_mpd_rds + (prd_mpd_rds * 2))
         except ZeroDivisionError:
             perc_dup_reads = 0
-    
+
         outdata = (unprd_mpd_rds, prd_mpd_rds, unprd_dup_rds, prd_dup_rds,
                    unmpd_rds, perc_dup_reads, est_lib_size)
-    
+
         outdata = tuple(['nan' if val == '' else val for val in outdata])
         return outdata
-    
+
     def extract_insert_metrics(self):
         ''' Extract median and mean insert size '''
-    
+
         # picardtools insertmetrics completes with code 0 and doesn't generate metrics file
         # if inputs don't have sufficient read count
         if not os.path.isfile(self.insert_metrics):
@@ -151,34 +152,41 @@ class CollectMetrics(object):
         #if the insert metrics fails due to low coverage
         if open(self.insert_metrics).readline().startswith("## FAILED"):
             return 0, 0, 0
-    
+
         mfile = open(self.insert_metrics)
-        
+
         targetlines = []
-        
+
         line = mfile.readline()
-        
+
         while line != '':
             if line.startswith('## METRICS CLASS'):
                 targetlines.append(mfile.readline().strip().split('\t'))
                 targetlines.append(mfile.readline().strip().split('\t'))
                 break
             line = mfile.readline()
-    
+
         mfile.close()
-    
+
         header, data = targetlines
-    
+
         header = [v.lower() for v in header]
         header = {v:i for i,v in enumerate(header)}
-    
+
         median_ins_size = data[header['median_insert_size']]
         mean_ins_size = data[header['mean_insert_size']]
         std_dev_ins_size = data[header['standard_deviation']]
-    
+
+        median_ins_size = 0 if median_ins_size == '?' else median_ins_size
+        mean_ins_size = 0 if mean_ins_size == '?' else mean_ins_size
+        std_dev_ins_size = 0 if std_dev_ins_size == '?' else std_dev_ins_size
+
         return median_ins_size, mean_ins_size, std_dev_ins_size
-    
-    
+
+    def merge_biobloom_metrics(self):
+        merged = pd.merge(pd.read_csv(self.output), pd.read_csv(self.biobloom_count_metrics), left_index=True, right_index=True)
+        merged.to_csv(self.output, index=False)
+
     def write_data(self, header, data):
         """
         write to the output
@@ -186,38 +194,41 @@ class CollectMetrics(object):
         assert len(header) == len(data)
         #replace empty vals with NA
         data = [v if v != '' else 'NA' for v in data]
-    
+
         writer = open(self.output, 'w')
         writer.write(','.join(header) + '\n')
         writer.write(','.join([str(v) for v in data]))
         writer.close()
-    
-    
+
+
     #=========================================================================
     # Run script
     #=========================================================================
-    
+
     def main(self):
-    
+
         duplication_metrics = self.extract_duplication_metrics()
         flagstat_metrics = self.extract_flagstat_metrics()
         wgs_metrics = self.extract_wgs_metrics()
-    
-        header = ['cell_id', 'unpaired_mapped_reads',
-                  'paired_mapped_reads', 'unpaired_duplicate_reads',
-                  'paired_duplicate_reads', 'unmapped_reads', 'percent_duplicate_reads',
-                  'estimated_library_size', 'total_reads', 'total_mapped_reads',
-                  'total_duplicate_reads', 'total_properly_paired',
-                  'coverage_breadth', 'coverage_depth']
+
+        header = [
+            'cell_id', 'unpaired_mapped_reads',
+            'paired_mapped_reads', 'unpaired_duplicate_reads',
+            'paired_duplicate_reads', 'unmapped_reads', 'percent_duplicate_reads',
+            'estimated_library_size', 'total_reads', 'total_mapped_reads',
+            'total_duplicate_reads', 'total_properly_paired',
+            'coverage_breadth', 'coverage_depth',
+        ]
 
         output = (self.sample_id,) + duplication_metrics + flagstat_metrics + wgs_metrics
-    
+
         if self.insert_metrics:
             insert_metrics = self.extract_insert_metrics()
             output += insert_metrics
             header += ['median_insert_size',
                        'mean_insert_size',
                        'standard_deviation_insert_size']
-    
+
         self.write_data(header, output)
+        self.merge_biobloom_metrics()
 
