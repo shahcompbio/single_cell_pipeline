e1bbddc9d21b183248218dd211351d46e64f7c92
Diljot Grewal <diljot.grewal@gmail.com>
2019-05-23 17:16:59 -0400

    minor
    Former-commit-id: 07bfe4029395d3ea2593ec4e3c52cbba45e86756

--- single_cell/workflows/align/__init__.py
+++ single_cell/workflows/align/__init__.py
@@ -67,7 +67,7 @@ def create_alignment_workflow(
     workflow.transform(
         name='merge_all_biobloom',
         func="single_cell.utils.csvutils.tasks.concatenate_csv",
-        args=(mgd.TempInputFile('biobloom_count_metrics', 'cell_id'),
+        args=(mgd.TempInputFile('biobloom_count_metrics_merged', 'cell_id'),
               mgd.OutputFile(biobloom_metrics),
               )
     )
