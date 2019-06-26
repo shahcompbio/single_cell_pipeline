903ae7861a9c14fb36f12be7fb64e4e53719d463
Diljot Grewal <diljot.grewal@gmail.com>
2019-06-26 15:29:16 -0400

    feature: rescaling corrupt tree plots, added bookmarks to merged pdf
    Former-commit-id: 4eac95fbc5165fa677374db3f4b94c2a5d81f50a

--- single_cell/utils/pdfutils.py
+++ single_cell/utils/pdfutils.py
@@ -4,20 +4,21 @@ Created on Feb 20, 2018
 @author: dgrewal
 '''
 
-
-from PyPDF2 import PdfFileMerger
-import helpers
 import os
 
-def merge_pdfs(infiles, outfile):
+from PyPDF2 import PdfFileMerger, PdfFileWriter, PdfFileReader
 
+import helpers
+
+
+def merge_pdfs(infiles, outfile):
     if isinstance(infiles, dict):
         infiles = infiles.values()
 
     merger = PdfFileMerger()
 
     for infile in infiles:
-        #add it to list if not empty. skip empty files to avoid errors later
+        # add it to list if not empty. skip empty files to avoid errors later
         if os.path.getsize(infile):
             merger.append(open(infile, 'rb'))
 
@@ -25,3 +26,29 @@ def merge_pdfs(infiles, outfile):
 
     with open(outfile, 'wb') as fout:
         merger.write(fout)
+
+
+def merge_pdfs_with_scaling(infiles, outfile, width=500, height=500):
+    if isinstance(infiles, dict):
+        infiles = infiles.values()
+
+    pdf_writer = PdfFileWriter()
+
+    pagenum = 0
+
+    for infile in infiles:
+        pdf_file = PdfFileReader(file(infile, 'rb'))
+        num_pages = pdf_file.getNumPages()
+
+        for page_number in range(0, num_pages):
+            pdf_page = pdf_file.getPage(page_number)
+
+            pdf_page.scaleTo(width, height)
+
+            pdf_writer.addPage(pdf_page)
+
+            pdf_writer.addBookmark(title=infile, pagenum=pagenum)
+            pagenum += 1
+
+    with open(outfile, 'wb') as fout:
+        pdf_writer.write(fout)
