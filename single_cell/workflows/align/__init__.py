596e09152ef7b7bb58e7c65651938d516573eee9
Diljot Grewal <diljot.grewal@gmail.com>
2019-07-02 12:02:40 -0400

    refactor: parts of alignment pipeline
    Former-commit-id: 2aa04bccac9abf8e15e2bf9f3503299aec4c11e8

--- single_cell/workflows/align/__init__.py
+++ single_cell/workflows/align/__init__.py
@@ -9,6 +9,200 @@ import pypeliner
 import pypeliner.managed as mgd
 
 
+def bam_metrics_workflow(
+        bam_filename,
+        summary_fastq_screen_count_per_cell,
+        alignment_metrics,
+        gc_metrics,
+        metrics_dir,
+        ref_genome,
+        sample_info,
+        config,
+        cell_ids
+):
+    markdups_metrics = os.path.join(
+        metrics_dir, 'markdups_metrics', '{cell_id}.markdups_metrics.txt'
+    )
+    flagstat_metrics = os.path.join(
+        metrics_dir, 'flagstat_metrics', '{cell_id}.flagstat_metrics.txt'
+    )
+    wgs_metrics_filename = os.path.join(
+        metrics_dir, 'wgs_metrics', '{cell_id}.wgs_metrics.txt'
+    )
+    gc_metrics_filename = os.path.join(
+        metrics_dir, 'gc_metrics', '{cell_id}.gc_metrics.txt'
+    )
+    gc_summary_filename = os.path.join(
+        metrics_dir, 'gc_metrics', '{cell_id}.gc_metrics.summ.txt'
+    )
+    gc_chart_filename = os.path.join(
+        metrics_dir, 'gc_metrics', '{cell_id}.gc_metrics.pdf'
+    )
+    insert_metrics_filename = os.path.join(
+        metrics_dir, 'insert_metrics', '{cell_id}.insert_metrics.txt'
+    )
+    insert_histogram_filename = os.path.join(
+        metrics_dir, 'insert_metrics', '{cell_id}.insert_metrics.pdf'
+    )
+
+    baseimage = config['docker']['single_cell_pipeline']
+    workflow = pypeliner.workflow.Workflow(ctx={'docker_image': baseimage})
+
+    workflow.setobj(
+        obj=mgd.OutputChunks('cell_id'),
+        value=cell_ids,
+    )
+
+    workflow.transform(
+        name='get_duplication_metrics',
+        axes=('cell_id',),
+        func="single_cell.utils.picardutils.bam_markdups",
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            mgd.TempOutputFile("temp_markdup_bam.bam", 'cell_id'),
+            mgd.OutputFile(markdups_metrics, 'cell_id'),
+            mgd.TempSpace('tempdir_markdups', 'cell_id'),
+        ),
+        kwargs={'docker_image': config['docker']['picard']}
+    )
+
+    workflow.transform(
+        name='get_flagstat_metrics',
+        axes=('cell_id',),
+        func="single_cell.utils.bamutils.bam_flagstat",
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            mgd.OutputFile(flagstat_metrics, 'cell_id'),
+        ),
+        kwargs={'docker_image': config['docker']['samtools']}
+    )
+
+    workflow.transform(
+        name='bam_collect_wgs_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.picardutils.bam_collect_wgs_metrics",
+        axes=('cell_id',),
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            ref_genome,
+            mgd.OutputFile(wgs_metrics_filename, 'cell_id'),
+            config['picard_wgs_params'],
+            mgd.TempSpace('wgs_tempdir', 'cell_id'),
+        ),
+        kwargs={
+            'docker_image': config['docker']['picard']
+        }
+    )
+
+    workflow.transform(
+        name='bam_collect_gc_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.picardutils.bam_collect_gc_metrics",
+        axes=('cell_id',),
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            ref_genome,
+            mgd.OutputFile(gc_metrics_filename, 'cell_id'),
+            mgd.OutputFile(gc_summary_filename, 'cell_id'),
+            mgd.OutputFile(gc_chart_filename, 'cell_id'),
+            mgd.TempSpace('gc_tempdir', 'cell_id'),
+        ),
+        kwargs={
+            'docker_image': config['docker']['picard']
+        }
+    )
+
+    workflow.transform(
+        name='bam_collect_insert_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.picardutils.bam_collect_insert_metrics",
+        axes=('cell_id',),
+        args=(
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            mgd.InputFile(flagstat_metrics, 'cell_id'),
+            mgd.OutputFile(insert_metrics_filename, 'cell_id'),
+            mgd.OutputFile(insert_histogram_filename, 'cell_id'),
+            mgd.TempSpace('insert_tempdir', 'cell_id'),
+        ),
+        kwargs={
+            'docker_image': config['docker']['picard']
+        }
+    )
+
+    workflow.transform(
+        name="collect_gc_metrics",
+        func="single_cell.workflows.align.tasks.collect_gc",
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        args=(
+            mgd.InputFile(gc_metrics_filename, 'cell_id', axes_origin=[]),
+            mgd.TempOutputFile('gc_metrics.csv.gz', extensions=['.yaml']),
+            mgd.TempSpace("temp_gc")
+        ),
+    )
+
+    workflow.transform(
+        name='collect_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.workflows.align.tasks.collect_metrics",
+        args=(
+            mgd.InputFile(flagstat_metrics, 'cell_id', axes_origin=[]),
+            mgd.InputFile(markdups_metrics, 'cell_id', axes_origin=[]),
+            mgd.InputFile(insert_metrics_filename, 'cell_id', axes_origin=[]),
+            mgd.InputFile(wgs_metrics_filename, 'cell_id', axes_origin=[]),
+            mgd.TempSpace("tempdir_collect_metrics"),
+            mgd.TempOutputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
+        ),
+    )
+
+    workflow.transform(
+        name='annotate_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.csvutils.annotate_metrics",
+        args=(
+            mgd.TempInputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
+            sample_info,
+            mgd.TempOutputFile('alignment_metrics_annotated.csv.gz', extensions=['.yaml']),
+        ),
+    )
+
+    workflow.transform(
+        name='add_fastqscreen_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.csvutils.merge_csv",
+        args=(
+            [
+                mgd.TempInputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
+                mgd.InputFile(summary_fastq_screen_count_per_cell),
+            ],
+            mgd.TempOutputFile('metrics_orgfilter_counts.csv.gz', extensions=['.yaml']),
+            'outer',
+            ['cell_id'],
+        ),
+    )
+
+    workflow.transform(
+        name='finalize_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.csvutils.finalize_csv",
+        args=(
+            mgd.TempInputFile('metrics_orgfilter_counts.csv.gz', extensions=['.yaml']),
+            mgd.OutputFile(alignment_metrics, extensions=['.yaml']),
+        ),
+    )
+
+    workflow.transform(
+        name='finalize_gc_metrics',
+        ctx={'mem': config['memory']['med'], 'ncpus': 1},
+        func="single_cell.utils.csvutils.finalize_csv",
+        args=(
+            mgd.TempInputFile('gc_metrics.csv.gz', extensions=['.yaml']),
+            mgd.OutputFile(gc_metrics, extensions=['.yaml']),
+        ),
+    )
+
+    return workflow
+
+
 def create_alignment_workflow(
         fastq_1_filename,
         fastq_2_filename,
@@ -64,7 +258,7 @@ def create_alignment_workflow(
         name='run_fastq_screen',
         ctx={'mem': 7, 'ncpus': 1, 'docker_image': baseimage},
         axes=('cell_id', 'lane',),
-        func="single_cell.workflows.align.tasks.organism_filter",
+        func="single_cell.workflows.align.fastqscreen.organism_filter",
         args=(
             mgd.InputFile('fastq_1', 'cell_id', 'lane', fnames=fastq_1_filename),
             mgd.InputFile('fastq_2', 'cell_id', 'lane', fnames=fastq_2_filename),
@@ -86,7 +280,7 @@ def create_alignment_workflow(
     workflow.transform(
         name='merge_fastq_screen_metrics',
         ctx={'mem': 7, 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.workflows.align.tasks.merge_fastq_screen_counts",
+        func="single_cell.workflows.align.fastqscreen.merge_fastq_screen_counts",
         args=(
             mgd.TempInputFile('organism_detailed_count_per_lane.csv', 'cell_id', 'lane'),
             mgd.TempInputFile('organism_summary_count_per_lane.csv', 'cell_id', 'lane'),
@@ -128,7 +322,6 @@ def create_alignment_workflow(
         )
     )
 
-
     workflow.transform(
         name='merge_bams',
         ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
@@ -155,7 +348,7 @@ def create_alignment_workflow(
                 mgd.TempInputFile('merged_lanes.bam', 'cell_id'),
                 mgd.TempInputFile('merged_lanes.bam.bai', 'cell_id'),
                 mgd.TempOutputFile('realigned.bam', 'chrom', 'cell_id'),
-                mgd.TempSpace('realignment_temp', 'chrom', cleanup='before'),
+                mgd.TempSpace('realignment_temp', 'chrom'),
                 config,
                 mgd.InputInstance('chrom')
             )
@@ -178,14 +371,6 @@ def create_alignment_workflow(
     if realign:
         final_bam = mgd.TempInputFile('merged_realign.bam', 'cell_id')
 
-    markdups_metrics = os.path.join(
-        metrics_dir,
-        'markdups_metrics',
-        '{cell_id}.markdups_metrics.txt')
-    flagstat_metrics = os.path.join(
-        metrics_dir,
-        'flagstat_metrics',
-        '{cell_id}.flagstat_metrics.txt')
     workflow.transform(
         name='postprocess_bam',
         ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
@@ -196,128 +381,23 @@ def create_alignment_workflow(
             mgd.OutputFile('sorted_markdups', 'cell_id', fnames=bam_filename, extensions=['.bai']),
             mgd.TempSpace('tempdir', 'cell_id'),
             config['docker'],
-            mgd.OutputFile(markdups_metrics, 'cell_id'),
-            mgd.OutputFile(flagstat_metrics, 'cell_id'),
         ),
     )
 
-    wgs_metrics_filename = os.path.join(
-        metrics_dir, 'wgs_metrics', '{cell_id}.wgs_metrics.txt')
-    workflow.transform(
-        name='bam_collect_wgs_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.workflows.align.tasks.bam_collect_wgs_metrics",
-        axes=('cell_id',),
+    workflow.subworkflow(
+        name='metrics_subworkflow',
+        func="single_cell.workflows.align.bam_metrics_workflow",
         args=(
-            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
+            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename, extensions=['.bai']),
+            mgd.TempInputFile('organism_summary_count_per_cell.csv'),
+            mgd.OutputFile(alignment_metrics, extensions=['.yaml']),
+            mgd.OutputFile(gc_metrics, extensions=['.yaml']),
+            metrics_dir,
             ref_genome,
-            mgd.OutputFile(wgs_metrics_filename, 'cell_id'),
-            config['docker'],
-            config['picard_wgs_params'],
-            mgd.TempSpace('wgs_tempdir', 'cell_id'),
-        ),
-    )
-
-    gc_metrics_filename = os.path.join(
-        metrics_dir,
-        'gc_metrics',
-        '{cell_id}.gc_metrics.txt')
-    gc_summary_filename = os.path.join(
-        metrics_dir,
-        'gc_metrics',
-        '{cell_id}.gc_metrics.summ.txt')
-    gc_chart_filename = os.path.join(
-        metrics_dir,
-        'gc_metrics',
-        '{cell_id}.gc_metrics.pdf')
-    workflow.transform(
-        name='bam_collect_gc_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.workflows.align.tasks.bam_collect_gc_metrics",
-        axes=('cell_id',),
-        args=(
-            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
-            ref_genome,
-            mgd.OutputFile(gc_metrics_filename, 'cell_id'),
-            mgd.OutputFile(gc_summary_filename, 'cell_id'),
-            mgd.OutputFile(gc_chart_filename, 'cell_id'),
-            mgd.TempSpace('gc_tempdir', 'cell_id'),
-            config['docker'],
-        ),
-    )
-
-    insert_metrics_filename = os.path.join(
-        metrics_dir,
-        'insert_metrics',
-        '{cell_id}.insert_metrics.txt')
-    insert_histogram_filename = os.path.join(
-        metrics_dir,
-        'insert_metrics',
-        '{cell_id}.insert_metrics.pdf')
-    workflow.transform(
-        name='bam_collect_insert_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.workflows.align.tasks.bam_collect_insert_metrics",
-        axes=('cell_id',),
-        args=(
-            mgd.InputFile('sorted_markdups', 'cell_id', fnames=bam_filename),
-            mgd.InputFile(flagstat_metrics, 'cell_id'),
-            mgd.OutputFile(insert_metrics_filename, 'cell_id'),
-            mgd.OutputFile(insert_histogram_filename, 'cell_id'),
-            mgd.TempSpace('insefefert_tempdir', 'cell_id'),
-            config['docker'],
-        ),
-    )
-
-    workflow.transform(
-        name="collect_gc_metrics",
-        func="single_cell.workflows.align.tasks.collect_gc",
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        args=(
-            mgd.InputFile(gc_metrics_filename, 'cell_id', axes_origin=[]),
-            mgd.TempOutputFile('gc_metrics.csv.gz', extensions=['.yaml']),
-            mgd.TempSpace("temp_gc")
-        ),
-    )
-
-    workflow.transform(
-        name='collect_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.workflows.align.tasks.collect_metrics",
-        args=(
-            mgd.InputFile(flagstat_metrics, 'cell_id', axes_origin=[]),
-            mgd.InputFile(markdups_metrics, 'cell_id', axes_origin=[]),
-            mgd.InputFile(insert_metrics_filename, 'cell_id', axes_origin=[]),
-            mgd.InputFile(wgs_metrics_filename, 'cell_id', axes_origin=[]),
-            mgd.TempSpace("tempdir_collect_metrics"),
-            mgd.TempOutputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
-        ),
-    )
-
-    workflow.transform(
-        name='annotate_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.workflows.align.tasks.annotate_metrics",
-        args=(
-            mgd.TempInputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
             sample_info,
-            mgd.TempOutputFile('alignment_metrics_annotated.csv.gz', extensions=['.yaml']),
-        ),
-    )
-
-    workflow.transform(
-        name='add_fastqscreen_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.utils.csvutils.merge_csv",
-        args=(
-            [
-                mgd.TempInputFile("alignment_metrics.csv.gz", extensions=['.yaml']),
-                mgd.TempInputFile('organism_summary_count_per_cell.csv'),
-            ],
-            mgd.TempOutputFile('alignment_metrics_fastq_screen_counts.csv.gz', extensions=['.yaml']),
-            'outer',
-            ['cell_id'],
-        ),
+            config,
+            cell_ids
+        )
     )
 
     workflow.transform(
@@ -325,32 +405,12 @@ def create_alignment_workflow(
         ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
         func="single_cell.workflows.align.tasks.plot_metrics",
         args=(
-            mgd.TempInputFile('alignment_metrics_annotated.csv.gz', extensions=['.yaml']),
+            mgd.InputFile(alignment_metrics, extensions=['.yaml']),
             mgd.OutputFile(plot_metrics),
             'QC pipeline metrics',
-            mgd.TempInputFile('gc_metrics.csv.gz', extensions=['.yaml']),
+            mgd.InputFile(gc_metrics, extensions=['.yaml']),
             config['gc_windows'],
         )
-    )
-
-    workflow.transform(
-        name='finalize_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.utils.csvutils.finalize_csv",
-        args=(
-            mgd.TempInputFile('alignment_metrics_fastq_screen_counts.csv.gz', extensions=['.yaml']),
-            mgd.OutputFile(alignment_metrics, extensions=['.yaml']),
-        ),
-    )
-
-    workflow.transform(
-        name='finalize_gc_metrics',
-        ctx={'mem': config['memory']['med'], 'ncpus': 1, 'docker_image': baseimage},
-        func="single_cell.utils.csvutils.finalize_csv",
-        args=(
-            mgd.TempInputFile('gc_metrics.csv.gz', extensions=['.yaml']),
-            mgd.OutputFile(gc_metrics, extensions=['.yaml']),
-        ),
     )
 
     return workflow
---
+++ single_cell/workflows/align/fastqscreen.py
@@ -0,0 +1,166 @@
+import os
+import shutil
+from collections import defaultdict
+
+import pandas as pd
+import pypeliner
+from single_cell.utils import helpers
+from single_cell.utils import csvutils
+from single_cell.utils import fastqutils
+
+
+def merge_fastq_screen_counts(
+        all_detailed_counts, all_summary_counts, merged_detailed_counts, merged_summary_counts
+):
+    if isinstance(all_detailed_counts, dict):
+        all_detailed_counts = all_detailed_counts.values()
+
+    detailed_data = [pd.read_csv(countsfile) for countsfile in all_detailed_counts]
+
+    df = pd.concat(detailed_data)
+
+    index_cols = [v for v in df.columns.values if v != "count"]
+
+    df['count'] = df.groupby(index_cols)['count'].transform('sum')
+
+    df = df.drop_duplicates(subset=index_cols)
+
+    csvutils.write_dataframe_to_csv_and_yaml(df, merged_detailed_counts, header=True)
+
+    if isinstance(all_summary_counts, dict):
+        all_summary_counts = all_summary_counts.values()
+
+    summary_counts = [pd.read_csv(countsfile) for countsfile in all_summary_counts]
+    df = pd.concat(summary_counts)
+
+    update_cols = [v for v in df.columns.values if v != 'cell_id']
+
+    for colname in update_cols:
+        df[colname] = df.groupby('cell_id')[colname].transform('sum')
+
+    df = df.drop_duplicates(subset=['cell_id'])
+
+    csvutils.write_dataframe_to_csv_and_yaml(df, merged_summary_counts, header=True)
+
+
+def run_fastq_screen_paired_end(fastq_r1, fastq_r2, tempdir, params, docker_image=None):
+    config = os.path.join(tempdir, 'fastq_screen.config')
+
+    with open(config, 'w') as config_writer:
+        for genome in params['genomes']:
+            genome_name = genome['name']
+            genome_path = genome['path']
+            outstr = '\t'.join(['DATABASE', genome_name, genome_path]) + '\n'
+            config_writer.write(outstr)
+
+    cmd = [
+        'fastq_screen',
+        '--aligner', params['aligner'],
+        '--conf', config,
+        '--outdir', tempdir,
+        '--tag',
+        fastq_r1,
+        fastq_r2,
+    ]
+
+    pypeliner.commandline.execute(*cmd, docker_image=docker_image)
+
+    basename = os.path.basename(fastq_r1)
+    basename = basename.split('.')[0]
+    tagged_fastq_r1 = os.path.join(tempdir, '{}.tagged.fastq.gz'.format(basename))
+
+    basename = os.path.basename(fastq_r2)
+    basename = basename.split('.')[0]
+    tagged_fastq_r2 = os.path.join(tempdir, '{}.tagged.fastq.gz'.format(basename))
+
+    return tagged_fastq_r1, tagged_fastq_r2
+
+
+def write_detailed_counts(counts, outfile, cell_id):
+    header = None
+
+    with helpers.getFileHandle(outfile, 'w') as writer:
+
+        for read_end, read_end_counts in counts.iteritems():
+
+            if not header:
+                outstr = ['cell_id', 'readend']
+                outstr += [v[0] for v in read_end_counts.keys()[0]]
+                outstr += ['count']
+                writer.write(','.join(outstr) + '\n')
+                header = 1
+
+            for flags, count in read_end_counts.iteritems():
+                outstr = [cell_id, read_end]
+                outstr += [v[1] for v in flags]
+                outstr += [count]
+                writer.write(','.join(map(str, outstr)) + '\n')
+
+
+def write_summary_counts(counts, outfile, cell_id):
+    summary_counts = defaultdict(int)
+    for read_end, read_end_counts in counts.iteritems():
+        for flags, count in read_end_counts.iteritems():
+            hit_orgs = [v[0] for v in flags if v[1] > 0]
+
+            for org in hit_orgs:
+                summary_counts[org] += count
+
+            if len(hit_orgs) > 1:
+                for org in hit_orgs:
+                    summary_counts['{}_multihit'.format(org)] += count
+            elif len(hit_orgs) == 0:
+                summary_counts['nohit'] += count
+
+    with helpers.getFileHandle(outfile, 'w') as writer:
+        keys = sorted(summary_counts.keys())
+        header = ['cell_id'] + keys
+        header = ','.join(header) + '\n'
+        writer.write(header)
+
+        values = [cell_id] + [summary_counts[v] for v in keys]
+        values = ','.join(map(str, values)) + '\n'
+        writer.write(values)
+
+
+def filter_reads(
+        input_r1, input_r2, output_r1, output_r2, reference,
+):
+    reader = fastqutils.PairedTaggedFastqReader(input_r1, input_r2)
+
+    with helpers.getFileHandle(output_r1, 'w') as writer_r1, helpers.getFileHandle(output_r2, 'w') as writer_r2:
+        for read_1, read_2 in reader.filter_read_iterator(reference):
+            for line in read_1:
+                writer_r1.write(line)
+
+            for line in read_2:
+                writer_r2.write(line)
+
+
+def organism_filter(
+        fastq_r1, fastq_r2, filtered_fastq_r1, filtered_fastq_r2,
+        detailed_metrics, summary_metrics, tempdir, cell_id, params,
+        reference, docker_image=None, no_organism_filter=False,
+):
+    helpers.makedirs(tempdir)
+
+    tagged_fastq_r1, tagged_fastq_r2 = run_fastq_screen_paired_end(
+        fastq_r1, fastq_r2, tempdir, params, docker_image=docker_image
+    )
+
+    reader = fastqutils.PairedTaggedFastqReader(tagged_fastq_r1, tagged_fastq_r2)
+    counts = reader.gather_counts()
+
+    write_detailed_counts(counts, detailed_metrics, cell_id)
+    write_summary_counts(counts, summary_metrics, cell_id)
+
+    if no_organism_filter:
+        # use the full tagged fastq downstream
+        # with organism type information in readname
+        shutil.copy(tagged_fastq_r1, filtered_fastq_r1)
+        shutil.copy(tagged_fastq_r2, filtered_fastq_r2)
+    else:
+        filter_reads(
+            tagged_fastq_r1, tagged_fastq_r2, filtered_fastq_r1,
+            filtered_fastq_r2, reference,
+        )
