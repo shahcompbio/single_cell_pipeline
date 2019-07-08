fd2458043bc5bff019219bfcb71a044bbbdcfb84
Diljot Grewal <diljot.grewal@gmail.com>
2019-07-08 13:02:05 -0400

    jenkins: updated docker build

---
+++ docker/single_cell_pipeline/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REGISTRY=$1
+
+TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
+
+docker build -t single_cell_pipeline -f docker/single_cell_pipeline/dockerfile .
+
+docker tag single_cell_pipeline $REGISTRY/scp/single_cell_pipeline:$TAG
+
+docker push $REGISTRY/scp/single_cell_pipeline:$TAG
