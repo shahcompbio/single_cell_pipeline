033508527cb85a470c6c4e53d85eb5f27c4faafc
Diljot Grewal <diljot.grewal@gmail.com>
2019-09-26 16:11:29 -0400

    refactor: updated main run layer

--- single_cell/run.py
+++ single_cell/run.py
@@ -1,21 +1,17 @@
 import sys
 
-from single_cell.aneufinder import aneufinder_pipeline
+from single_cell.alignment import alignment_pipeline
+from single_cell.annotation import annotation_pipeline
 from single_cell.breakpoint_calling import breakpoint_calling_pipeline
 from single_cell.clean_sentinels import clean_sentinels
 from single_cell.cmdline import parse_args
-from single_cell.copy_number import copy_number_calling_pipeline
 from single_cell.docker_run import run_with_docker
 from single_cell.generate_config import generate_config
-from single_cell.germline_calling import germline_calling_pipeline
+from single_cell.hmmcopy import hmmcopy_pipeline
 from single_cell.infer_haps import infer_haps_pipeline
-from single_cell.ltm import ltm_pipeline
 from single_cell.merge_bams import merge_bams_pipeline
-from single_cell.multi_sample import multi_sample_pipeline
-from single_cell.qc import qc_pipeline
 from single_cell.split_bam import split_bam_pipeline
 from single_cell.variant_calling import variant_calling_pipeline
-from single_cell.variant_calling import variant_counting_pipeline
 
 
 def main():
@@ -33,11 +29,14 @@ def main():
         run_with_docker(args, sys.argv)
         return
 
-    if args["which"] == "qc":
-        qc_pipeline(args)
+    if args["which"] == "alignment":
+        alignment_pipeline(args)
 
-    if args["which"] == "aneufinder":
-        aneufinder_pipeline(args)
+    if args["which"] == "hmmcopy":
+        hmmcopy_pipeline(args)
+
+    if args["which"] == "annotation":
+        annotation_pipeline(args)
 
     if args["which"] == "merge_bams":
         merge_bams_pipeline(args)
@@ -48,26 +47,11 @@ def main():
     if args["which"] == "variant_calling":
         variant_calling_pipeline(args)
 
-    if args["which"] == "copy_number_calling":
-        copy_number_calling_pipeline(args)
-
     if args["which"] == "infer_haps":
         infer_haps_pipeline(args)
 
-    if args["which"] == "germline_calling":
-        germline_calling_pipeline(args)
-
     if args["which"] == "breakpoint_calling":
         breakpoint_calling_pipeline(args)
-
-    if args["which"] == "variant_counting":
-        variant_counting_pipeline(args)
-
-    if args["which"] == "ltm":
-        ltm_pipeline(args)
-
-    if args["which"] == "multi_sample_pseudo_bulk":
-        multi_sample_pipeline(args)
 
 
 if __name__ == "__main__":
