c504e132089461d77fe6c99eb5a35adba46e1e68
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-11-19 22:14:29 -0500

    docker: single cell pipeline docker updates

--- docker/single_cell_pipeline/run.sh
+++ docker/single_cell_pipeline/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 REGISTRY=$1
-
-sed -i "s/{container_registry}/$1/g" docker/single_cell_pipeline/dockerfile
-
+GIT_COMMIT=`git rev-parse HEAD`
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
+
+cat docker/single_cell_pipeline/dockerfile_template \
+ | sed "s/{container_registry}/$REGISTRY/g" \
+ | sed "s/{git_commit}/$GIT_COMMIT/g" \
+ > docker/single_cell_pipeline/dockerfile
 
 docker build -t single_cell_pipeline -f docker/single_cell_pipeline/dockerfile .
 
