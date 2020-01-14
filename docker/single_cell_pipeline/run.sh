d399471f8e41935016655296c6c4b511039ee6a5
Diljot Grewal <diljot.grewal@gmail.com>
2020-01-14 11:16:17 -0500

    minor: install the tagged commit in docker container

--- docker/single_cell_pipeline/run.sh
+++ docker/single_cell_pipeline/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 REGISTRY=$1
-GIT_COMMIT=`git rev-parse HEAD`
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 
 cat docker/single_cell_pipeline/dockerfile_template \
  | sed "s/{container_registry}/$REGISTRY/g" \
- | sed "s/{git_commit}/$GIT_COMMIT/g" \
+ | sed "s/{git_commit}/$TAG/g" \
  > docker/single_cell_pipeline/dockerfile
 
 docker build -t single_cell_pipeline -f docker/single_cell_pipeline/dockerfile .
