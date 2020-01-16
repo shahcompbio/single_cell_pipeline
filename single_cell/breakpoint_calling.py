b62a426c3cfdd612ad76bc9491755864d549e4d8
Diljot Grewal <diljot.grewal@gmail.com>
2020-01-16 14:00:59 -0500

    bkp calling: flags to disable parts of wf
    Former-commit-id: 6e96b5e7fd71d3ab23ffe5fabced6bd463b25a25

--- single_cell/breakpoint_calling.py
+++ single_cell/breakpoint_calling.py
@@ -1,21 +1,23 @@
 import os
 import sys
 
+import pypeliner
 import pypeliner.managed as mgd
 from single_cell.utils import inpututils
 
-import pypeliner
 
+def get_output_files(bkp_dir, run_destruct, run_lumpy):
+    data = {}
 
-def get_output_files(bkp_dir):
-    data = {
-        'destruct_breakpoints_filename': os.path.join(bkp_dir, 'destruct_breakpoints.csv.gz'),
-        'destruct_breakpoints_lib_filename': os.path.join(bkp_dir, 'destruct_breakpoints_library.csv.gz'),
-        'destruct_cell_counts_filename': os.path.join(bkp_dir, 'destruct_cell_counts.csv.gz'),
-        'lumpy_breakpoints_bed': os.path.join(bkp_dir, 'lumpy_breakpoints.bed'),
-        'lumpy_breakpoints_csv': os.path.join(bkp_dir, 'lumpy_breakpoints.csv.gz'),
-        'lumpy_breakpoints_evidence_csv': os.path.join(bkp_dir, 'lumpy_breakpoints_evidence.csv.gz'),
-    }
+    if run_destruct:
+        data['destruct_breakpoints_filename'] = os.path.join(bkp_dir, 'destruct_breakpoints.csv.gz')
+        data['destruct_breakpoints_lib_filename'] = os.path.join(bkp_dir, 'destruct_breakpoints_library.csv.gz')
+        data['destruct_cell_counts_filename'] = os.path.join(bkp_dir, 'destruct_cell_counts.csv.gz')
+
+    if run_lumpy:
+        data['lumpy_breakpoints_bed'] = os.path.join(bkp_dir, 'lumpy_breakpoints.bed')
+        data['lumpy_breakpoints_csv'] = os.path.join(bkp_dir, 'lumpy_breakpoints.csv.gz')
+        data['lumpy_breakpoints_evidence_csv'] = os.path.join(bkp_dir, 'lumpy_breakpoints_evidence.csv.gz')
 
     return data
 
@@ -24,13 +26,20 @@ def breakpoint_calling_workflow(args):
     config = inpututils.load_config(args)
     config = config['breakpoint_calling']
 
+    run_destruct = True if args['destruct'] else False
+    run_lumpy = True if args['lumpy'] else False
+
+    if not run_destruct and not run_lumpy:
+        run_destruct = True
+        run_lumpy = True
+
     normal_data, tumour_cells = inpututils.load_breakpoint_calling_input(args['input_yaml'])
 
     bkp_dir = os.path.join(args['out_dir'])
     bkp_meta = os.path.join(args['out_dir'], 'metadata.yaml')
     input_yaml_blob = os.path.join(args['out_dir'], 'input.yaml')
 
-    out_files = get_output_files(bkp_dir)
+    out_files = get_output_files(bkp_dir, run_destruct, run_lumpy)
 
     ref_data_directory = config['ref_data_directory']
 
@@ -55,34 +64,36 @@ def breakpoint_calling_workflow(args):
     else:
         normal_bam = mgd.InputFile(normal_data, extensions=['.bai'])
 
-    workflow.subworkflow(
-        name='destruct',
-        ctx={'docker_image': config['docker']['single_cell_pipeline']},
-        func="single_cell.workflows.destruct_singlecell.create_destruct_workflow",
-        args=(
-            normal_bam,
-            mgd.InputFile('tumour.bam', 'tumour_cell_id', fnames=tumour_cells),
-            config.get('destruct_config', {}),
-            config,
-            ref_data_directory,
-            mgd.OutputFile(out_files['destruct_breakpoints_filename'], extensions=['.yaml']),
-            mgd.OutputFile(out_files['destruct_breakpoints_lib_filename'], extensions=['.yaml']),
-            mgd.OutputFile(out_files['destruct_cell_counts_filename'], extensions=['.yaml']),
-        ),
-    )
+    if run_destruct:
+        workflow.subworkflow(
+            name='destruct',
+            ctx={'docker_image': config['docker']['single_cell_pipeline']},
+            func="single_cell.workflows.destruct_singlecell.create_destruct_workflow",
+            args=(
+                normal_bam,
+                mgd.InputFile('tumour.bam', 'tumour_cell_id', fnames=tumour_cells),
+                config.get('destruct_config', {}),
+                config,
+                ref_data_directory,
+                mgd.OutputFile(out_files['destruct_breakpoints_filename'], extensions=['.yaml']),
+                mgd.OutputFile(out_files['destruct_breakpoints_lib_filename'], extensions=['.yaml']),
+                mgd.OutputFile(out_files['destruct_cell_counts_filename'], extensions=['.yaml']),
+            ),
+        )
 
-    workflow.subworkflow(
-        name='lumpy',
-        func="single_cell.workflows.lumpy.create_lumpy_workflow",
-        args=(
-            config,
-            normal_bam,
-            mgd.InputFile('tumour.bam', 'tumour_cell_id', fnames=tumour_cells, extensions=['.bai']),
-            mgd.OutputFile(out_files['lumpy_breakpoints_csv'], extensions=['.yaml']),
-            mgd.OutputFile(out_files['lumpy_breakpoints_evidence_csv'], extensions=['.yaml']),
-            mgd.OutputFile(out_files['lumpy_breakpoints_bed']),
-        ),
-    )
+    if run_lumpy:
+        workflow.subworkflow(
+            name='lumpy',
+            func="single_cell.workflows.lumpy.create_lumpy_workflow",
+            args=(
+                config,
+                normal_bam,
+                mgd.InputFile('tumour.bam', 'tumour_cell_id', fnames=tumour_cells, extensions=['.bai']),
+                mgd.OutputFile(out_files['lumpy_breakpoints_csv'], extensions=['.yaml']),
+                mgd.OutputFile(out_files['lumpy_breakpoints_evidence_csv'], extensions=['.yaml']),
+                mgd.OutputFile(out_files['lumpy_breakpoints_bed']),
+            ),
+        )
 
     workflow.transform(
         name='generate_meta_files_results',
