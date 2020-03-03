cd8c4770107905fee17208a647cd533ab29c8fe8
Douglas Abrams <abramsd@mskcc.org>
2020-03-03 13:24:55 -0500

    testing: alignment, hmmcopy, variant calling, breakpoint calling, annotation, split, merge, count_haps
    Former-commit-id: b9a7ab161fdeae077b30e946f007df06937b3eff

--- tests/jenkins/merge_cell_bams/merge_cell_bams.sh
+++ tests/jenkins/merge_cell_bams/merge_cell_bams.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 
-
-mkdir -p MERGE_CELL_BAMS/bams/
+mkdir -p MERGE_CELL_BAMS/ref_test_data
 
 docker run -v $PWD:$PWD -w $PWD singlecellpipeline/azurecli:v0.0.1 \
-  az storage blob download-batch -s merge-bams -d MERGE_CELL_BAMS/bams/ --account-name $1 --account-key $2
-
+  az storage blob download-batch -s merge-bams -d MERGE_CELL_BAMS/ref_test_data --account-name $1 --account-key $2
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
-  singlecellpipeline/single_cell_pipeline:$TAG \
+  $3/single_cell_pipeline:$TAG \
   single_cell merge_cell_bams --input_yaml tests/jenkins/merge_cell_bams/inputs.yaml \
   --maxjobs 4 --nocleanup --sentinel_only  \
-  --context_config tests/jenkins/merge_cell_bams/context_config.yaml \
+  --context_config tests/jenkins/align/context_config.yaml \
   --submit local --loglevel DEBUG \
   --tmpdir MERGE_CELL_BAMS/temp \
   --pipelinedir MERGE_CELL_BAMS/pipeline --submit local --out_dir MERGE_CELL_BAMS/output
+
+docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker --rm \
+  $3/single_cell_pipeline:$TAG \
+  python tests/jenkins/merge_cell_bams/test_merge_cell_bams.py MERGE_CELL_BAMS/output MERGE_CELL_BAMS/ref_test_data/refdata
