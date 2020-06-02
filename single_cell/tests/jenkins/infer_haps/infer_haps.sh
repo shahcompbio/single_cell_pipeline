021dd96599fecf974d8d895db7c25e0c346bc5e4
Diljot Grewal <diljot.grewal@gmail.com>
2020-06-02 14:20:40 -0400

    testing: infer haps

--- single_cell/tests/jenkins/infer_haps/infer_haps.sh
+++ single_cell/tests/jenkins/infer_haps/infer_haps.sh
@@ -6,15 +6,15 @@ TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 TAG="${TAG}.beta"
 
 
-mkdir -p INFER_HAPS/bam
+mkdir -p INFER_HAPS/ref_test_data
 
 docker run -v $PWD:$PWD -w $PWD singlecellpipeline/azurecli:v0.0.1 \
-  az storage blob download-batch -s infer-haps  -d INFER_HAPS/bam/ --account-name $1 --account-key $2
+  az storage blob download-batch -s infer-haps  -d INFER_HAPS/ref_test_data/ --account-name $1 --account-key $2
 
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
-  singlecellpipeline/single_cell_pipeline:$TAG \
+  $3/single_cell_pipeline:$TAG \
   single_cell infer_haps --input_yaml single_cell/tests/jenkins/infer_haps/inputs.yaml \
   --maxjobs 4 --nocleanup --sentinel_only  \
   --context_config single_cell/tests/jenkins/context_config.yaml \
@@ -23,6 +23,12 @@ docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/va
   --pipelinedir INFER_HAPS/pipeline \
   --submit local \
   --out_dir INFER_HAPS/output \
-  --config_override '{"infer_haps":{"chromosomes":["15"], "extract_seqdata": {"genome_fai_template": "/refdata/human/infer_haps/GRCh37-lite.fa.fai", "genome_fasta_template": "/refdata/human/infer_haps/GRCh37-lite.fa"}, "ref_data_dir": "/refdata/human/infer_haps/"} "version": '\"$TAG\"'}' \
+  --config_override '{"infer_haps":{"chromosomes":["15"], "extract_seqdata": {"genome_fai_template": "/refdata/human/infer_haps/GRCh37-lite.fa.fai", "genome_fasta_template": "/refdata/human/infer_haps/GRCh37-lite.fa"}, "ref_data_dir": "/refdata/human/infer_haps/"}, "version": '\"$TAG\"'}' \
+
+
+docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker --rm \
+  $3/single_cell_pipeline:$TAG \
+  python single_cell/tests/jenkins/infer_haps/test_infer_haps.py INFER_HAPS/output INFER_HAPS/ref_test_data
 
 docker run -w $PWD -v $PWD:$PWD --rm $3/single_cell_pipeline:$TAG rm -rf INFER_HAPS
