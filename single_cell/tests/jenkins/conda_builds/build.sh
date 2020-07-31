c955c2a87cbe10437cdce5055aec34f668a78b53
root <root@dlvioncpliner1.mskcc.org>
2020-07-31 12:28:15 -0400

    build: stick to py37 for now, conda packages not available for py38

--- single_cell/tests/jenkins/conda_builds/build.sh
+++ single_cell/tests/jenkins/conda_builds/build.sh
@@ -1,9 +1,9 @@
 set -e
 
-docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.2 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build -c conda-forge -c bioconda -c shahcompbio conda/single_cell_pipeline_align"
+docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build -c conda-forge -c bioconda -c shahcompbio conda/single_cell_pipeline_align"
 
-docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.2 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_hmmcopy -c r -c conda-forge -c bioconda -c shahcompbio -c dranew"
+docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_hmmcopy -c r -c conda-forge -c bioconda -c shahcompbio -c dranew"
 
-docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.2 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_annotation -c conda-forge -c bioconda -c shahcompbio"
+docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_annotation -c conda-forge -c bioconda -c shahcompbio"
 
 
