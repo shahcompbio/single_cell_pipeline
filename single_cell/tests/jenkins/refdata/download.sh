af7ad51514a2abd599373a1904138740fee0ec8a
Diljot <diljot.grewal@gmail.com>
2020-11-19 14:46:13 -0800

    v0.6.35: supports azure-storage-blob v12 (#104)
    * v0.6.35: supports azure-storage-blob v12
    
    * minor: switch to quay.io

--- single_cell/tests/jenkins/refdata/download.sh
+++ single_cell/tests/jenkins/refdata/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -d "/refdata" ]; then
-    docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v /refdata:/refdata singlecellpipeline/awscli:v0.0.1 aws s3 cp s3://singlecelltestsets/TESTDATA_CODEBUILD/refdata /refdata --recursive
+    docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v /refdata:/refdata quay.io/singlecellpipeline/awscli:v0.0.1 aws s3 cp s3://singlecelltestsets/TESTDATA_CODEBUILD/refdata /refdata --recursive
 fi
 
