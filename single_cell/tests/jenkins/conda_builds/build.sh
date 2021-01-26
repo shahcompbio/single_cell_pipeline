da2ef74df18996a3de271d120797d1d814060ecd
Diljot Grewal <diljot.grewal@gmail.com>
2021-01-26 11:18:17 -0800

    build: hmmcopy now needs conda-forge

--- single_cell/tests/jenkins/conda_builds/build.sh
+++ single_cell/tests/jenkins/conda_builds/build.sh
@@ -2,7 +2,7 @@ set -e
 
 docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build -c bioconda -c shahcompbio conda/single_cell_pipeline_align"
 
-docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_hmmcopy -c bioconda -c shahcompbio"
+docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_hmmcopy -c bioconda -c shahcompbio -c conda-forge"
 
 docker run -e ANACONDA_API_TOKEN  -v $PWD:$PWD -w $PWD singlecellpipeline/conda_build:v0.0.1 bash -c "conda update -n base -c defaults conda -y &&  conda config --set anaconda_upload yes && conda build conda/single_cell_pipeline_annotation  -c shahcompbio"
 
