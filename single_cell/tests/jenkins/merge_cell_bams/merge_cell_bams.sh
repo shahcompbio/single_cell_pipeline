5252bc934089213a9e3375a6336bd330279dabad
diljotgrewwal <diljot.grewal@gmail.com>
2021-03-01 13:22:00 -0500

    CI: added dockerfiles and updated codebuild

--- single_cell/tests/jenkins/merge_cell_bams/merge_cell_bams.sh
+++ single_cell/tests/jenkins/merge_cell_bams/merge_cell_bams.sh
@@ -14,17 +14,15 @@ docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v $DOCKER:$DOCKER --rm \
-  $1/single_cell_pipeline:$TAG \
+  $1/single_cell_pipeline_alignment:$TAG \
   single_cell merge_cell_bams \
   --input_yaml single_cell/tests/jenkins/merge_cell_bams/inputs.yaml \
   --maxjobs $NUMCORES --nocleanup --sentinel_only  \
-  --context_config single_cell/tests/jenkins/context_config.yaml \
   --submit local --loglevel DEBUG \
   --tmpdir MERGE_CELL_BAMS/temp \
   --pipelinedir MERGE_CELL_BAMS/pipeline \
   --submit local \
   --out_dir MERGE_CELL_BAMS/output \
-  --config_override '{ "version": '\"$TAG\"'}'
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v $DOCKER:$DOCKER --rm \
--- single_cell/tests/jenkins/variant_calling/variant_calling.sh
+++ single_cell/tests/jenkins/variant_calling/variant_calling.sh
@@ -13,7 +13,7 @@ docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v $DOCKER:$DOCKER --rm \
-  $1/single_cell_pipeline:$TAG \
+  $1/single_cell_pipeline_variant:$TAG \
   single_cell variant_calling --input_yaml single_cell/tests/jenkins/variant_calling/inputs.yaml \
   --maxjobs $NUMCORES --nocleanup --sentinel_only  \
   --context_config single_cell/tests/jenkins/context_config.yaml \
