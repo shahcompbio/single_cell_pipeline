f502085d87d73ff1841fdb698d3dff22c0e8352a
Diljot Grewal <diljot.grewal@gmail.com>
2021-03-05 10:43:11 -0800

    v0.7.0: docker updates

--- conda/single_cell_pipeline_annotation/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build and install python package
-cd $SRC_DIR/single_cell_pipeline
-$PYTHON setup.py install
-
-cd $SRC_DIR/cell_cycle_classifier
-$PYTHON setup.py install
--- conda/single_cell_pipeline_hmmcopy/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Build and install python package
-$PYTHON setup.py install
