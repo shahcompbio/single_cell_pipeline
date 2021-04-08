3e8ae1aaa64670f989309b9c117b531b462690a7
Diljot Grewal <diljot.grewal@gmail.com>
2021-04-08 09:42:27 -0700

    codebuild updates

---
+++ single_cell/tests/jenkins/snv_genotyping/variant_calling.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
+DOCKER=`which docker`
+NUMCORES=`nproc --all`
+
+mkdir -p SNV_GENOTYPING/testdata
+
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v $PWD:$PWD -w $PWD $1/awscli:v0.0.1 \
+  aws s3 cp s3://singlecelltestsets/TESTDATA_CODEBUILD/snv_genotyping VARIANT_CALLING/testdata/ --recursive
+
+docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $DOCKER:$DOCKER --rm \
+  $1/single_cell_pipeline_variant:$TAG \
+  single_cell snv_genotyping --input_yaml single_cell/tests/jenkins/snv_genotyping/inputs.yaml \
+  --maxjobs $NUMCORES --nocleanup --sentinel_only  \
+  --submit local --loglevel DEBUG \
+  --tmpdir SNV_GENOTYPING/temp \
+  --pipelinedir SNV_GENOTYPING/pipeline --submit local --out_dir SNV_GENOTYPING/output \
+  --config_override '{"variant_calling": {"chromosomes": ["6", "8", "17"]}, "version": '\"$TAG\"'}'
+
+docker run -w $PWD -v $PWD:$PWD --rm $1/single_cell_pipeline_variant:$TAG rm -rf SNV_GENOTYPING
