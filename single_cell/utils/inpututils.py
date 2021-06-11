fef6df897c840557277709f147659aa088074b92
Douglas Abrams <abramsd@ceto.cbio.private>
2021-06-11 14:13:54 -0400

    changes to input loading

--- single_cell/utils/inpututils.py
+++ single_cell/utils/inpututils.py
@@ -1,4 +1,4 @@
-import yaml
+mport yaml
 from single_cell.utils.validator import validate
 
 
@@ -162,13 +162,15 @@ def load_yaml(path):
 def load_qc_input(path):
     data = {}
     yaml = load_yaml(path)
+    assert len(yaml.keys()) == 1
+    patient = list(yaml.keys())[0]
     for patient, patient_data in yaml.items():
         for sample, sample_data in patient_data.items():
             for library, library_data in sample_data.items():
-                data[(patient, sample, library)] = {
+                data[(sample, library)] = {
                     data_label: data for data_label, data in library_data.items()
                 }
-    return data
+    return data, patient
 
 
 def get_sample_info(fastqs_file):
@@ -236,18 +238,18 @@ def get_fastqs(fastqs_file):
 
 def load_cohort_qc_inputs(yaml_file):
     yaml_data = load_yaml(yaml_file)
+    cohort = list(yaml_data.keys())[0]
+    data = yaml_data[cohort]
+    germline_mafs = {}
+    vcfs = {}
+    hmmcopy = {}
 
-    mafs = {}
-    hmmcopy_filenames = {}
-    hmmcopy_samples = {}
+    for sample, data in data.items():
+        germline_mafs[sample] = data["germline_maf"]
+        for library, data in data["libdata"].items():
+            hmmcopy[(sample, library)] = {"hmmcopy": data["hmmcopy_reads"]}
+            vcfs[(sample, library)] = {"museq": data["museq"],
+                                       "strelka_snv": data["strelka_snv"],
+                                       "strelka_indel": data["strelka_indel"]}
 
-    for sample, data in yaml_data["hmmcopy"].items():
-        for library, data in data.items():
-            hmmcopy_filenames[(sample, library)] = data["hmmcopy_reads"]
-            hmmcopy_samples[(sample, library)] = data["sample_id"]
-
-    for sample, data in yaml_data["mafs"].items():
-        mafs[(sample)] = data
-
-    return mafs, hmmcopy_filenames, hmmcopy_samples
-
+    return cohort, germline_mafs, vcfs, hmmcopy
