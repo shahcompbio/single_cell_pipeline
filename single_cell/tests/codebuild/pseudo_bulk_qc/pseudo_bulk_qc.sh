1edbd2112e44a4077d63cce5ac1225ee7196cfc2
Diljot Grewal <diljot.grewal@gmail.com>
2021-06-14 17:15:35 -0700

    codebuild: error

--- single_cell/tests/codebuild/pseudo_bulk_qc/pseudo_bulk_qc.sh
+++ single_cell/tests/codebuild/pseudo_bulk_qc/pseudo_bulk_qc.sh
@@ -13,7 +13,7 @@ docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata --rm \
   $1/single_cell_pipeline_qc:$TAG \
-  single_cell pseudo_bulk_qc --input_yaml single_cell/tests/codebuild/pseudo_bulk_qc/inputs.yaml \
+  single_cell sample_qc --input_yaml single_cell/tests/codebuild/pseudo_bulk_qc/inputs.yaml \
   --maxjobs $NUMCORES --nocleanup --sentinel_only  \
   --submit local --loglevel DEBUG \
   --tmpdir PSEUDO_BULK_QC/temp \
