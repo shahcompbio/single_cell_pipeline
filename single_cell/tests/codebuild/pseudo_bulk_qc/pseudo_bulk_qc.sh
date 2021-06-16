b3a09a2b53d25b29483734963a8863312a462b68
diljotgrewwal <diljot.grewal@gmail.com>
2021-06-16 14:08:48 -0400

    teest

--- single_cell/tests/codebuild/pseudo_bulk_qc/pseudo_bulk_qc.sh
+++ single_cell/tests/codebuild/pseudo_bulk_qc/pseudo_bulk_qc.sh
@@ -9,7 +9,7 @@ NUMCORES=`nproc --all`
 mkdir -p PSEUDO_BULK_QC/ref_test_data
 
 docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v $PWD:$PWD -w $PWD $1/awscli:v0.0.1 \
-  aws s3 cp s3://singlecelltestsets/TESTDATA_CODEBUILD/pseudo_bulk_qc PSEUDO_BULK_QC/ref_test_data --recursive --quiet
+  aws s3 cp s3://singlecelltestsets/TESTDATA_CODEBUILD/sample_qc PSEUDO_BULK_QC/ref_test_data --recursive --quiet
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata --rm \
   $1/single_cell_pipeline_qc:$TAG \
