74ba4b931a36aadacd21b399b03f1a1e1fcfc80e
Diljot Grewal <diljot.grewal@gmail.com>
2022-05-11 17:15:17 -0700

    fastqc needs 2 threads or else it hangs

--- single_cell/workflows/align/scripts/run_trimgalore.py
+++ single_cell/workflows/align/scripts/run_trimgalore.py
@@ -117,6 +117,8 @@ class RunTrimGalore(object):
         if self.adapter2:
             cmd.extend(['--adapter2', self.adapter2])
 
+        cmd.extend(['--fastqc_args', '--threads', '2'])
+
         cmd.extend([self.seq1, self.seq2, ])
 
         self.run_cmd(cmd)
